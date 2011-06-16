442628c9d25a48f56bc703ad75e5c2c075c73414
Valentin Haenel <valentin.haenel@gmx.de>
2011-06-16 14:01:09 +0200

    NF: implement a skeleton universe incl. layout
    Tried to write the universe, but noticed that I need to be able to specify the
    universe-layout somehow. I basically used a similar method as before. The layout
    is specified as a string with special symbols. I also do some extensive legality
    checking here to avoid future fuckups.
    
    Also the simple executions should be refactored into unit-tests.

---
+++ pelita/universe.py
@@ -0,0 +1,131 @@
+wall   = '#'
+food   = '.'
+harvester = 'c'
+destroyer = 'o'
+
+layout_chars = [wall, food, harvester, destroyer]
+
+north = 'NORTH'
+south = 'SOUTH'
+west  = 'WEST'
+east  = 'EAST'
+
+class LayoutEncodingException(Exception):
+    pass
+
+def check_layout(layout_str, number_bots):
+    """ Check the legality of the layout string.
+
+    Parameters
+    ----------
+    layout_str : str
+        the layout string
+    number_bots : int
+        the total number of bots that should be present
+
+    Raises
+    ------
+    LayoutEncodingException
+        if an illegal character is encountered
+    LayoutEncodingException
+        if a bot-id is missing
+    LayoutEncodingException
+        if a bot-id is specified twice
+
+    """
+    bot_ids = [str(i) for i in range(1, number_bots+1)]
+    existing_bots = []
+    legal = layout_chars + bot_ids  + [' ', '\n']
+    for c in layout_str:
+        if c not in legal:
+            raise LayoutEncodingException(
+                "Char: '%c' is not a legal layout character" % c)
+        if c in bot_ids:
+            if c in existing_bots:
+                raise LayoutEncodingException(
+                    "Bot-ID: '%c' was specified twice" % c)
+            else:
+                existing_bots.append(c)
+    if bot_ids != existing_bots:
+        missing = [str(i) for i in set(bot_ids).difference(set(existing_bots))]
+        missing.sort()
+        raise LayoutEncodingException(
+            'Layout is invalid for %i Bots, The following IDs were missing: %s '
+            % (number_bots, missing))
+
+def strip_layout(layout_str):
+    """ Remove whitespace from a string encoded layout
+
+    Parameters
+    ----------
+    layout_str : str
+        the layout, possibly with whitespace
+
+    Returns
+    -------
+    layout_str : str
+        the layout with whitespace removed
+
+    """
+    return ''.join([line.strip()+'\n' for line in layout_str.split('\n')])
+
+class Universe(object):
+
+    def __init__():
+        pass
+
+    def set_layout(layout):
+        self.layout = layout
+
+    def get_layout():
+        return self.layout
+
+    def init_bots():
+        pass
+
+    def get_number_bots():
+        pass
+
+    def move_bot(index, move):
+        pass
+
+    def reset_bot(index):
+        pass
+
+if __name__ == "__main__":
+    test_layout = """#######
+                     #c    #
+                     #  .  #
+                     #    o#
+                     #######"""
+    print strip_layout(test_layout)
+
+    illeagal_layout = """#######
+                         #c    #
+                         #  f  #
+                         #    o#
+                         #######"""
+    try:
+        check_layout(strip_layout(illeagal_layout), 0)
+    except Exception, e:
+        print e
+
+    not_enough_bots = """#######
+                         #1    #
+                         #  2  #
+                         #    3#
+                         #######"""
+    try:
+        check_layout(strip_layout(not_enough_bots), 5)
+    except Exception, e:
+        print e
+
+    too_many_bots = """#######
+                       #1    #
+                       #  1  #
+                       #    3#
+                       #######"""
+    try:
+        check_layout(strip_layout(too_many_bots), 3)
+    except Exception, e:
+        print e
