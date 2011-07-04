b224fac2f76a18c3547804961062729becbe7ea3
Valentin Haenel <valentin.haenel@gmx.de>
2011-07-04 16:10:48 +0200

    RF: move demo game to a demo script

--- pelita/game_master.py
+++ pelita/game_master.py
@@ -65,17 +65,3 @@ class RandomPlayer(AbstractPlayer):
         legal_moves = universe.get_legal_moves(universe.bots[self.index].current_pos)
         return random.choice(legal_moves.keys())
 
-if __name__ == '__main__':
-    layout = (
-        """ ##################
-            #0#.  .  # .     #
-            #2#####    #####1#
-            #     . #  .  .#3#
-            ################## """)
-    gm = GameMaster(layout, 4, 200)
-    gm.register_player(0, RandomPlayer(0))
-    gm.register_player(1, RandomPlayer(1))
-    gm.register_player(2, RandomPlayer(2))
-    gm.register_player(3, RandomPlayer(3))
-    gm.register_viewer(AsciiViewer())
-    gm.play()
