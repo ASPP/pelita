b3f3989c1799aebe22ba33a54bda81ea082573ee
Valentin Haenel <valentin.haenel@gmx.de>
2011-07-14 18:14:10 +0200

    ENH: add lots of convenience to the AbstractPlayer

--- pelita/player.py
+++ pelita/player.py
@@ -51,6 +51,19 @@ class AbstractPlayer(object):
     def get_move(self, universe):
         raise NotImplementedError(
                 "You must override the 'get_move' method in your player")
+
+    @property
+    def current_uni(self):
+        """ The current Universe.
+
+        Returns
+        -------
+        universe : Universe
+            the current Universe
+
+        """
+        return self.universe_states[-1]
+
     @property
     def me(self):
         """ The Bot object this Player controls.
@@ -59,9 +72,58 @@ class AbstractPlayer(object):
         -------
         me : Bot
             the bot controlled by this player
-        """
-        self.universe_states[-1].bots[self._index]
 
+        """
+        self.current_uni.bots[self._index]
+
+    @property
+    def team_bots(self):
+        """ A list of Bots that are on this players team.
+
+        Returns
+        -------
+        team_mates : list of Bot objects
+            the team mates
+
+        """
+        this_team = [self.current_uni.bots[i] for i in
+            self.current_uni.teams[me._index].bots]
+        this_team.pop(self._index)
+        return this_team
+
+    @property
+    def enemy_bots(self):
+        """ A list of enemy Bots.
+
+        Returns
+        -------
+        enemy_bots : list of Bot objects
+            all Bots on all enemy teams
+        """
+        return self.current_uni.enemy_bots(self.me.team_index)
+
+    @property
+    def current_pos(self):
+        """ The current position of this bot.
+
+        Returns
+        -------
+        current_pos : tuple of (int, int)
+            the current position (x, y) of this bot
+        """
+        return self.me.current_pos
+
+    @property
+    def initial_pos(self):
+        """ The initial_pos of this bot.
+
+        Returns
+        -------
+        initial_pos : tuple of (int, int)
+            the initial position (x, y) of this bot
+
+        """
+        return me.initial_pos
 
 class StoppingPlayer(AbstractPlayer):
     """ A Player that just stands still. """
