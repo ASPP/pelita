e0072c416311077248f6a64e360fcacece8f711c
Valentin Haenel <valentin.haenel@gmx.de>
2011-07-14 16:39:23 +0200

    NF/DOC: docstrings for Player objects

--- pelita/player.py
+++ pelita/player.py
@@ -7,31 +7,71 @@ import random
 class AbstractPlayer(object):
 
     def _set_index(self, index):
+        """ Called by the GameMaster to set this Players index.
+
+        Parameters
+        ----------
+        index : int
+            this players index
+
+        """
         self._index = index
 
     def _set_initial(self, universe):
+        """ Called by the GameMaster on initialisation.
+
+        Parameters
+        ----------
+        universe : Universe
+            the initial state of the universe
+
+        """
         self.universe_states = []
         self.universe_states.append(universe)
 
     def set_initial(self, universe):
+        """ Subclasses can override this if desired. """
         pass
 
     def _get_move(self, universe):
+        """ Called by the GameMaster to obtain next move.
+
+        This will add the universe to the list of universe_states and then call
+        `self.get_move()`.
+
+        Parameters
+        ----------
+        universe : Universe
+            the universe in its current state.
+
+        """
         self.universe_states.append(universe)
         return self.get_move(universe)
 
     def get_move(self, universe):
         raise NotImplementedError(
                 "You must override the 'get_move' method in your player")
+    @property
+    def me(self):
+        """ The Bot object this Player controls.
+
+        Returns
+        -------
+        me : Bot
+            the bot controlled by this player
+        """
+        self.universe_states[-1].bots[self._index]
 
 
 class StoppingPlayer(AbstractPlayer):
+    """ A Player that just stands still. """
 
     def get_move(self, universe):
         return stop
 
 
 class RandomPlayer(AbstractPlayer):
+    """ A player that makes moves at random. """
 
     def get_move(self, universe):
         legal_moves = universe.get_legal_moves(
