8264f8f142f49255d2842458b63aa7e0ab95f689
Valentin Haenel <valentin.haenel@gmx.de>
2011-07-29 13:32:36 +0200

    ENH: check also for correct coding

--- check_boilerplate.sh
+++ check_boilerplate.sh
@@ -8,6 +8,14 @@ check_docformat(){
     fi
 }
 
+check_coding(){
+    if ! grep -q -e 'coding: utf-8' $1 ; then
+        echo "warning: file '$1' does not conatin a coding declaration!"
+    fi
+}
+
+
 for f in $( ls pelita/**/*.py ) ; do
     check_docformat $f
+    check_coding $f
 done
