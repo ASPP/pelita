463ead27253fa3f40b2b6dbe8e241a3ceef763d6
Rike-Benjamin Schuppner <rikebs@debilski.de>
2011-08-01 01:51:16 +0200

    ENH: Re-doing basic animation. Still buggy.

--- pelita/ui/tk_canvas.py
+++ pelita/ui/tk_canvas.py
@@ -71,13 +71,14 @@ class UiCanvas(object):
             datamodel.Food: Food
         }
 
+        self.waiting_animations = []
+
     def init_canvas(self):
         self.canvas = Canvas(self.master, width=self.mesh_graph.width, height=self.mesh_graph.height)
         self.canvas.pack(fill=BOTH, expand=YES)
         self.canvas.bind('<Configure>', self.resize)
 
-    def update(self, universe):
-        print "update"
+    def update(self, events=None, universe=None):
         if not self.canvas:
             if not self.mesh_graph:
                 width = universe.maze.width
@@ -90,11 +91,49 @@ class UiCanvas(object):
             self.init_canvas()
             self.init_bots(universe)
 
-        self.mesh_graph.num_x = universe.maze.width
-        self.mesh_graph.num_y = universe.maze.height
-        self.clear()
-        self.draw_mesh(universe.maze)
-        self.draw_bots(universe)
+        if universe:
+            self.mesh_graph.num_x = universe.maze.width
+            self.mesh_graph.num_y = universe.maze.height
+
+        if events:
+            move_events = events.filter_type(datamodel.BotMoves)
+            for move_event in move_events:
+                bot_idx = move_event.bot_index
+                bot_sprite = self.bot_sprites[bot_idx]
+
+                old_pos = complex(*bot_sprite.position).conjugate()
+                new_pos = complex(*universe.bots[bot_idx].current_pos).conjugate()
+
+                direction = new_pos - old_pos
+
+                arc = math.degrees(cmath.phase(direction))
+
+                self.waiting_animations += [
+                    Animation.rotate_to(self, bot_sprite, arc, duration=0.1),
+                    Animation.move(self, bot_sprite, (direction.real, - direction.imag), duration=0.1)
+                ]
+
+        if self.waiting_animations:
+            for animation in self.waiting_animations:
+                if not animation.start_time:
+                    animation.start()
+
+                animation.step()
+                self.canvas.update_idletasks()
+
+            # delete finished
+            self.waiting_animations = [anim for anim in self.waiting_animations if not anim.is_finished]
+
+            # come back again
+            self.canvas.after(10, self.update, None, universe)
+            return
+
+        if universe:
+            self.waiting_animations = []
+            self.clear()
+            self.draw_mesh(universe.maze)
+            self.draw_bots(universe)
+
 
     def clear(self):
         self.canvas.delete(ALL)
@@ -111,7 +150,17 @@ class UiCanvas(object):
 
     def init_bots(self, universe):
         for bot in universe.bots:
-            self.bot_sprites[bot.index] = BotSprite(self.mesh_graph)
+            bot_sprite = BotSprite(self.mesh_graph)
+
+            self.bot_sprites[bot.index] = bot_sprite
+            bot_sprite.position = bot.current_pos
+
+            if bot.is_harvester:
+                bot_sprite.bot_type = Harvester # Harvester(self.mesh_graph)
+            else:
+                bot_sprite.bot_type = Destroyer # (self.mesh_graph)
+
+            bot_sprite.score = universe.teams[bot.team_index].score
 
     def draw_bots(self, universe):
         for bot_idx, bot_sprite in self.bot_sprites.iteritems():
@@ -156,8 +205,6 @@ class TkApplication(Frame):
         self.pack(fill=BOTH, expand=YES)
         self.ui_canvas = UiCanvas(self)
 
-        self.animations = []
-
     def read_queue(self):
         try:
             # read all events.
@@ -174,28 +221,7 @@ class TkApplication(Frame):
         universe = observed["universe"]
         events = observed["events"]
 
-        self.ui_canvas.update(universe)
-
-    def redraw(self):
-        for anim in self.animations:
-            anim.step()
-
-        self.canvas.canvas.update_idletasks()
-        self.after(10, self.redraw)
-
-    def react(self, event):
-        direction = event
-        pos = complex(direction[0], - direction[1])
-        arc = math.degrees(cmath.phase(pos))
-
-        anim_seq = [
-            Animation.rotate_to(canvas, canvas.registered_items[8], arc),
-            Animation.move(canvas, canvas.registered_items[8], direction),
-        ]
-        [anim.start() for anim in anim_seq]
-        self.animations += anim_seq
-
-        print direction
+        self.ui_canvas.update(events, universe)
 
     def on_quit(self):
         """ override for things which must be done when we exit.
@@ -212,7 +238,13 @@ class Animation(object):
     def __init__(self, duration):
         self.duration = duration
         self.start_time = None
-        self.is_finished = False
+
+    @property
+    def is_finished(self):
+        if self.start_time:
+            if self.elapsed() / self.duration >= 1:
+                return True
+        return False
 
     def elapsed(self):
         if self.start_time is None:
@@ -224,8 +256,7 @@ class Animation(object):
 
     def rate(self):
         rate = self.elapsed() / self.duration
-        if rate > 1:
-            self.is_finished = True
+        if rate >= 1:
             return 1
         return rate
 
@@ -266,8 +297,8 @@ class Animation(object):
     @classmethod
     def move(cls, canvas, item, transpos, duration=0.5):
         anim = cls(duration)
-        dx = float(transpos[0]) * canvas.mesh_graph.rect_height
-        dy = float(transpos[1]) * canvas.mesh_graph.rect_width
+        dx = float(transpos[0])
+        dy = float(transpos[1])
         anim.old_pos_x = item.position[0]
         anim.old_pos_y = item.position[1]
 
