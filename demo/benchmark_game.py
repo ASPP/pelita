3e747b26377c216e5927878602f1059b108da663
Tiziano Zito <opossumnano@gmail.com>
2011-08-10 16:20:48 +0200

    ENH: added benchmark game.
    This game is to be used to test the universe copying speed.

---
+++ demo/benchmark_game.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python
+from pelita.game_master import GameMaster
+from pelita.player import StoppingPlayer, SimpleTeam
+from pelita.viewer import AsciiViewer
+
+LAYOUT="""
+##################################
+#...   #      .#     #  #       3#
+# ## #   # ###    #  #     #####1#
+#.   # #    # .   # ##           #
+#.#    #  .    #    .  # #########
+# ## # ## ####    # ##.   . .   .#
+#.. .  .  #. . #. #  # ## #####  #
+# ## #### #.## #     #  .  . . ..#
+#..  ..   # #  #  #    ##### #####
+##### #####    #  #  # #   ..  ..#
+#.. . .  .  #     # ##.# #### ## #
+#  ##### ## #  # .# . .#  .  . ..#
+#.   . .   .## #    #### ## # ## #
+######### #  .    #    .  #    #.#
+#           ## #   . #    # #   .#
+#0#####     #  #    ### #   # ## #
+#2       #  #     #.      #   ...#
+##################################
+##################################
+#...   #      .#     #  #        #
+# ## #   # ###    #  #     ##### #
+#.   # #    # .   # ##           #
+#.#    #  .    #    .  # #########
+# ## # ## ####    # ##.   . .   .#
+#.. .  .  #. . #. #  # ## #####  #
+# ## #### #.## #     #  .  . . ..#
+#..  ..   # #  #  #    ##### #####
+##### #####    #  #  # #   ..  ..#
+#.. . .  .  #     # ##.# #### ## #
+#  ##### ## #  # .# . .#  .  . ..#
+#.   . .   .## #    #### ## # ## #
+######### #  .    #    .  #    #.#
+#           ## #   . #    # #   .#
+# #####     #  #    ### #   # ## #
+#        #  #     #.      #   ...#
+##################################
+"""
+
+if __name__ == '__main__':
+    import time
+    start = time.time()
+    layout = LAYOUT
+    gm = GameMaster(layout, 4, 10)
+    gm.register_team(SimpleTeam(StoppingPlayer(), StoppingPlayer()))
+    gm.register_team(SimpleTeam(StoppingPlayer(), StoppingPlayer()))
+    #gm.register_viewer(AsciiViewer())
+    gm.play()
+    print len(LAYOUT), time.time() - start
