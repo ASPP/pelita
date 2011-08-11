a5890d17ac5de35cf0c60eb60680fdae3d9f15ba
Valentin Haenel <valentin.haenel@gmx.de>
2011-08-11 16:49:47 +0200

    BF/ENH: enhance the demo and fix two bugs

--- demo/demo_defensive.py
+++ demo/demo_defensive.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
     with open('layouts/01.layout') as layout_file:
         layout = layout_file.read()
     gm = GameMaster(layout, 4, 200)
-    gm.register_team(SimpleTeam(BFSPlayer(), StoppingPlayer()))
-    gm.register_team(SimpleTeam(BasicDefensePlayer(), StoppingPlayer()))
+    gm.register_team(SimpleTeam(BFSPlayer(), BFSPlayer()))
+    gm.register_team(SimpleTeam(BasicDefensePlayer(), BasicDefensePlayer()))
     gm.register_viewer(AsciiViewer())
     gm.play()
