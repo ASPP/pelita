a0103884f48ac59a1f430739469297fa992c3fe1
Rike-Benjamin Schuppner <rikebs@debilski.de>
2011-08-12 17:17:42 +0200

    BF: Fixing the imports in __init__

--- pelita/__init__.py
+++ pelita/__init__.py
@@ -4,6 +4,9 @@ import pelita.datamodel
 import pelita.containers
 import pelita.layout
 import pelita.game_master
+import pelita.viewer
+import pelita.actors
+import pelita.simplesetup
 import pelita.__version_from_git
 
 __docformat__ = "restructuredtext"
