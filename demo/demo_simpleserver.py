ee51e66eff9735ce5a3b9a3ed436e3a821ce7de2
Rike-Benjamin Schuppner <rikebs@debilski.de>
2011-08-12 16:32:24 +0200

    DOC: Documentation for the simple demos.

--- demo/demo_simpleserver.py
+++ demo/demo_simpleserver.py
@@ -1,7 +1,19 @@
 # -*- coding: utf-8 -*-
 
+"""
+We start a server using the SimpleServer class and a layout
+given by the specified layout file.
+"""
+
 from pelita.simplesetup import SimpleServer
 
 server = SimpleServer(layoutfile="layouts/02.layout")
+# For more control, we could also define a game with 8 Bots,
+# run 10000 rounds and want to receive connections on port
+#
+# server = SimpleServer(layoutfile="layouts/02.layout", players=8, rounds=10000, port=61009)
+
+# Now we run it with a Tk interface.
+# This will only ever return if interrupted or if Tk is closed.
 server.run_tk()
 
