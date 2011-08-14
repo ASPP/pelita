5cd7cba4552cfa5f659a93b8f2ee5918e48f53c7
Rike-Benjamin Schuppner <rikebs@debilski.de>
2011-08-14 16:48:23 +0200

    BF: Fix against fork bomb on Windows.

--- demo/demo_simplegame.py
+++ demo/demo_simplegame.py
@@ -14,39 +14,45 @@ There is a smaller problem with this: If we start the client before the server,
 our client might try to connect before the server is ready. This is ‘fixed’
 repeated tries and should not be a problem in general.
 
+On certain operating systems (at least on Windows), starting processes naïvely
+may spark a fork bomb. Therefore, all scripts using multiprocessing must use
+a __name__=="__main__" part to shield from multiple execution of the script
+when a new process is started.
+
 """
 
 from pelita.simplesetup import SimpleClient, SimpleServer
 from pelita.player import RandomPlayer, BFSPlayer, SimpleTeam, BasicDefensePlayer, NQRandomPlayer
 
-client = SimpleClient("the good ones", SimpleTeam(NQRandomPlayer(), BFSPlayer()))
-client.autoplay_background()
+if __name__=="__main__":
+    client = SimpleClient("the good ones", SimpleTeam(NQRandomPlayer(), BFSPlayer()))
+    client.autoplay_background()
 
-client2 = SimpleClient("the bad ones", SimpleTeam(BFSPlayer(), BasicDefensePlayer()))
-client2.autoplay_background()
+    client2 = SimpleClient("the bad ones", SimpleTeam(BFSPlayer(), BasicDefensePlayer()))
+    client2.autoplay_background()
 
 
-layout = """
-##################################
-#...   #      .#     #  #       3#
-# ## #   # ###    #  #     #####1#
-#.   # #    # .   # ##           #
-#.#    #  .    #    .  # #########
-# ## # ## ####    # ##.   . .   .#
-#.. .  .  #. . #. #  # ## #####  #
-# ## #### #.## #     #  .  . . ..#
-#..  ..   # #  #  #    ##### #####
-##### #####    #  #  # #   ..  ..#
-#.. . .  .  #     # ##.# #### ## #
-#  ##### ## #  # .# . .#  .  . ..#
-#.   . .   .## #    #### ## # ## #
-######### #  .    #    .  #    #.#
-#           ## #   . #    # #   .#
-#0#####     #  #    ### #   # ## #
-#2       #  #     #.      #   ...#
-##################################
-"""
+    layout = """
+    ##################################
+    #...   #      .#     #  #       3#
+    # ## #   # ###    #  #     #####1#
+    #.   # #    # .   # ##           #
+    #.#    #  .    #    .  # #########
+    # ## # ## ####    # ##.   . .   .#
+    #.. .  .  #. . #. #  # ## #####  #
+    # ## #### #.## #     #  .  . . ..#
+    #..  ..   # #  #  #    ##### #####
+    ##### #####    #  #  # #   ..  ..#
+    #.. . .  .  #     # ##.# #### ## #
+    #  ##### ## #  # .# . .#  .  . ..#
+    #.   . .   .## #    #### ## # ## #
+    ######### #  .    #    .  #    #.#
+    #           ## #   . #    # #   .#
+    #0#####     #  #    ### #   # ## #
+    #2       #  #     #.      #   ...#
+    ##################################
+    """
 
-server = SimpleServer(layout=layout, rounds=3000)
-server.run_tk()
+    server = SimpleServer(layout=layout, rounds=3000)
+    server.run_tk()
 
