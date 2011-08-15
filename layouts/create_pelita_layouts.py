47866997abe5ee88097d65683daf2a613211385d
Tiziano Zito <opossumnano@gmail.com>
2011-08-15 15:30:12 +0200

    ENH: automatically convert layout directory to python file in pelita namespace

---
+++ layouts/create_pelita_layouts.py
@@ -0,0 +1,27 @@
+import os
+import pelita
+
+EXTENSION = '.layout'
+OUTFILENAME = 'layouts.py'
+
+local_dir = os.path.dirname(os.path.realpath(__file__))
+pelita_path = os.path.dirname(pelita.__file__)
+outfile = os.path.join(pelita_path, OUTFILENAME)
+
+content = ''
+# loop through all layout files
+for f in sorted(os.listdir(local_dir)):
+    flname, ext = os.path.splitext(f)
+    if ext != EXTENSION:
+        continue
+    with open(os.path.join(local_dir,f), 'r') as text:
+        layout = text.read()
+        # create string variable
+        content += 'layout_' + flname
+        # encode layout string
+        content += ' = """' + layout.encode('zlib').encode('base64') + '"""\n'
+
+# write out file in pelita directory
+with open(outfile, 'w') as out:
+    out.write(content)
+
