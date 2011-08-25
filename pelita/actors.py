8dddf06b5fe5702801c6994f90ecd76dab8ac385
Rike-Benjamin Schuppner <rikebs@debilski.de>
2011-08-25 17:49:48 +0200

    RF: connect_local() now calls connect() to reduce code duplication.

--- pelita/actors.py
+++ pelita/actors.py
@@ -121,22 +121,15 @@ class ClientActor(object):
         """ Tells our local actor to establish a local connection
         with other local actor `main_actor`.
         """
-        print "Trying to establish a connection with local actor '%s'..." % main_actor,
-        sys.stdout.flush()
-
-        try:
-            res = self.actor_ref.query("say_hello", [main_actor, self.team_name]).get(TIMEOUT)
-            print res
-            if res == "ok":
-                return True
-        except Queue.Empty:
-            print "failed due to timeout in actor."
-        return False
+        self.connect(main_actor, None, None)
 
     def connect(self, main_actor, host="", port=50007):
         """ Tells our local actor to establish a connection with `main_actor`.
         """
-        print "Trying to establish a connection with remote actor '%s'..." % main_actor,
+        if port is None:
+            print "Trying to establish a connection with local actor '%s'..." % main_actor,
+        else:
+            print "Trying to establish a connection with remote actor '%s'..." % main_actor,
         sys.stdout.flush()
 
         try:
