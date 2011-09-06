d894555e066dc0501f195926e2a66f69f9a12ef7
Valentin Haenel <valentin.haenel@gmx.de>
2011-09-06 15:24:11 +0200

    FO: reformat demo.py

--- demo.py
+++ demo.py
@@ -2,24 +2,26 @@
 # -*- coding: utf-8 -*-
 
 
-"""
-This file demonstrates setting up a server and two clients using local actor connections.
-The order is important in this case (as is described in demo_simplegame.py).
+""" This file demonstrates setting up a server and two clients using local actor
+connections.  The order is important in this case (as is described in
+demo_simplegame.py).
 
 A difference to the remote game in demo_simplegame is that now,
 `client.autoplay_background` uses a background thread instead of a background
 process. This background thread sometimes does not close on CTRL+C. In these
-cases, pressing CTRL+Z and then entering ‘kill %%’ usually is the way to
-get rid of the program.
-"""
+cases, pressing CTRL+Z and then entering ‘kill %%’ usually is the way to get rid
+of the program. """
 
 from pelita.simplesetup import SimpleClient, SimpleServer
-from pelita.player import RandomPlayer, BFSPlayer, SimpleTeam, StoppingPlayer, NQRandomPlayer, BasicDefensePlayer
+from pelita.player import RandomPlayer, NQRandomPlayer,\
+    BFSPlayer, BasicDefensePlayer, SimpleTeam
 
-client = SimpleClient(SimpleTeam("the good ones", RandomPlayer(), RandomPlayer()))
+client = SimpleClient(
+        SimpleTeam("the good ones", RandomPlayer(), NQRandomPlayer()))
 client.autoplay_background()
 
-client2 = SimpleClient(SimpleTeam("the bad ones", BFSPlayer(), BasicDefensePlayer()))
+client2 = SimpleClient(
+        SimpleTeam("the bad ones", BFSPlayer(), BasicDefensePlayer()))
 client2.autoplay_background()
 
 server = SimpleServer(rounds=3000)
