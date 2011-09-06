272f9f9d897a44a670ed539d2055613b96ed13cd
Valentin Haenel <valentin.haenel@gmx.de>
2011-09-06 17:55:26 +0200

    ENH/RF: make the loading of layouts from __layouts more flexible

--- pelita/layout.py
+++ pelita/layout.py
@@ -4,7 +4,7 @@
 import random
 
 from pelita.containers import Mesh
-import pelita.layouts as layouts
+import pelita.__layouts as __layouts
 
 __docformat__ = "restructuredtext"
 
@@ -22,12 +22,43 @@ def get_random_layout():
         a random layout string
 
     """
-    # loop in layouts dictionary and look for layout strings
-    layouts_str = [item for item in dir(layouts) if item.startswith('layout_')]
-    layout = random.choice(layouts_str)
-    # decode and return this layout
-    return layouts.__dict__[layout].decode('base64').decode('zlib')
+    layouts_names = get_available_layouts()
+    layout_choice = random.choice(layouts_names)
+    return get_layout_by_name(layout_choice)
 
+def get_available_layouts():
+    """ The names of the available layouts.
+
+    Returns
+    -------
+    layout_names : list of str
+        the available layouts
+
+    """
+    # loop in layouts dictionary and look for layout strings
+    return [item for item in dir(__layouts) if item.startswith('layout_')]
+
+def get_layout_by_name(layout_name):
+    """ Get an available layout.
+
+    Parameters
+    ----------
+    layout_name : str
+        a valid layout name
+
+    Returns
+    -------
+    layout_str : str
+        the layout as a string
+
+    Raises
+    ------
+    KeyError
+        if the layout_name is not available
+
+    """
+    # decode and return this layout
+    return __layouts.__dict__[layout_name].decode('base64').decode('zlib')
 
 class Layout(object):
     """ Auxiliary class to parse string encodings of mazes.
