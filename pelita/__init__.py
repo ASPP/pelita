702cee1e0ece01eecee257b733c1d7a1c89c7560
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2011-09-11 19:36:43 +0100

    Fix imports in pelita.py.

--- pelita/__init__.py
+++ pelita/__init__.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8 -*-
 
-import pelita.datamodel
-import pelita.containers
-import pelita.layout
-import pelita.game_master
-import pelita.viewer
-import pelita.actors
-import pelita.simplesetup
-import pelita.__version_from_git
+from . import (actors,
+               containers,
+               datamodel,
+               game_master,
+               layout,
+               player,
+               simplesetup,
+               viewer,
+               __version_from_git)
 
 __docformat__ = "restructuredtext"
 
