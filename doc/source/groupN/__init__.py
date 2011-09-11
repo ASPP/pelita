77d7b8ed2deea7122b8b67bf1642547c070f328e
Valentin Haenel <valentin.haenel@gmx.de>
2011-09-11 15:03:57 +0100

    DOC/BF: fix relative import

--- doc/source/groupN/__init__.py
+++ doc/source/groupN/__init__.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 from pelita.player import SimpleTeam
-from groupN.my_player import MyPlayer
+from .my_player import MyPlayer
 
 def factory():
     return SimpleTeam("My Team", MyPlayer(), MyPlayer())
