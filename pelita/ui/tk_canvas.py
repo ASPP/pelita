8538246ea55b867d0299ef6e17ed00ef8f615668
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2011-09-12 14:41:49 +0100

    FIX/UI: re-introduce the lambda to print status.
    This reverts commit a2bf136c7c82202330881c3c9f3ab0970539dfa5.
    
    The lambda was actually needed, but just the order of arguments
    needed to be fixed.

--- pelita/ui/tk_canvas.py
+++ pelita/ui/tk_canvas.py
@@ -99,6 +99,7 @@ class Trafo(object):
 class UiCanvas(object):
     def __init__(self, master, geometry=None):
         self.game_finish_overlay = lambda: None
+        self.game_status_info = lambda: None
 
         self.mesh_graph = None
         self.geometry = geometry
@@ -172,7 +173,8 @@ class UiCanvas(object):
             self.current_universe = universe
 
         if round is not None and turn is not None:
-            self.draw_status_info(turn, round)
+            self.game_status_info = lambda: self.draw_status_info(turn, round)
+        self.game_status_info()
 
         self.draw_universe(self.current_universe)
 
