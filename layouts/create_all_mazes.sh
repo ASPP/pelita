2854884fea34b7ba213aa0194690beff0afed8c8
Pietro Berkes <pietro.berkes@googlemail.com>
2011-09-13 11:41:18 +0100

    FIX: Changed layout naming convention to work well with name filtering

--- layouts/create_all_mazes.sh
+++ layouts/create_all_mazes.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
 SEED=39285
-N=3
+N=100
 
 for COUNT in $(seq -w 1 $N);
 do
    SEED=$(expr $SEED + 27)
    echo normal $COUNT
    ./create_maze.py --seed=$SEED --no-dead-ends --height=16 --width=32 > normal_$COUNT.layout
-   ./create_maze.py --seed=$SEED --dead-ends --height=16 --width=32 > "normal_"$COUNT"_dead_ends.layout"
+   ./create_maze.py --seed=$SEED --dead-ends --height=16 --width=32 > "normal_dead_ends_"$COUNT".layout"
 done
 
 for COUNT in $(seq -w 1 $N);
@@ -16,7 +16,7 @@ do
    SEED=$(expr $SEED + 27)
    echo small $COUNT
    ./create_maze.py --seed=$SEED --no-dead-ends --food=10 --height=8 --width=18 > small_$COUNT.layout
-   ./create_maze.py --seed=$SEED --dead-ends --food=10 --height=8 --width=18 > "small_"$COUNT"_dead_ends.layout"
+   ./create_maze.py --seed=$SEED --dead-ends --food=10 --height=8 --width=18 > "small_dead_ends_"$COUNT".layout"
 done
 
 for COUNT in $(seq -w 1 $N);
@@ -24,5 +24,5 @@ do
    SEED=$(expr $SEED + 27)
    echo big $COUNT
    ./create_maze.py --seed=$SEED --no-dead-ends --food=60 --height=32 --width=64 > big_$COUNT.layout
-   ./create_maze.py --seed=$SEED --dead-ends --food=60 --height=32 --width=64 > "big_"$COUNT"_dead_ends.layout"
+   ./create_maze.py --seed=$SEED --dead-ends --food=60 --height=32 --width=64 > "big_dead_ends_"$COUNT".layout"
 done
