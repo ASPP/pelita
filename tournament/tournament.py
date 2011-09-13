c9d9056461d239c1b4a9471723d74fbb69583661
Bastian Venthur <venthur@debian.org>
2011-09-13 16:06:16 +0200

    Removed participants, we rely on that the teams are in subdirectory of tournamed named exatly: group0 .. group4.

--- tournament/tournament.py
+++ tournament/tournament.py
@@ -10,19 +10,7 @@ POINTS_DRAW = 1
 POINTS_WIN = 2
 
 # FIXME: fit that for tournatment
-CMD_STUB = 'python pelitagame.py --rounds=10 --null'
-
-
-def get_teams():
-    """Read participants.txt and return a list with the team names."""
-    result = []
-    with open('participants.txt', 'r') as fh:
-        for line in fh.readlines():
-            line = line.strip()
-            if line.startswith('#') or len(line) == 0:
-                continue
-            result.append(line)
-    return result
+CMD_STUB = 'python ../pelitagame --rounds=100 --null'
 
 
 def organize_first_round(nr_teams):
@@ -37,12 +25,27 @@ def start_match(team1, team2):
     """Start a match between team1 and team2. Return which team won (1 or 2) or
     0 if there was a draw.
     """
+    print
+    print team1, 'vs', team2
+    print
     args = CMD_STUB.split()
     args.extend([team1, team2])
     print 'Starting', ' '.join(args)
-    stdout, stderr = Popen(args, cwd='..', stdout=PIPE, stderr=PIPE).communicate()
+    stdout, stderr = Popen(args, stdout=PIPE, stderr=PIPE).communicate()
     tmp = reversed(stdout.splitlines())
     lastline = None
+    # get the real names of the teams.
+    # pelitagame will output two lines of the following form:
+    # Using factory 'RandomPlayer' -> 'The RandomPlayers'
+    rname1, rname2 = '', ''
+    for line in stdout.splitlines():
+        if line.startswith("Using factory '"):
+            split = line.split("'")
+            tname, rname = split[1], split[3]
+            if tname == team1:
+                rname1 = rname
+            if tname == team2:
+                rname2 = rname
     for line in tmp:
         if line.startswith('Finished.'):
             lastline = line
@@ -53,26 +56,27 @@ def start_match(team1, team2):
         print stderr
         print "***"
         return 0
+    if rname1 == '' or rname2 == '':
+        print "*** ERROR: Could not find out the realnames of the teams, have to count that as a draw."
+        return 0
+    print "***", lastline
+    if lastline.find('had a draw.') >= 0:
+        print "Draw!"
+        return 0
     else:
-        print "***", lastline 
-        if lastline.find('had a draw.') >= 0:
-            print "Draw!"
-            return 0
+        tmp = lastline.split("'")
+        # FIXME: the names in the output do *not* match the names in the participants file!
+        winner = tmp[1]
+        loser = tmp[3]
+        if winner == rname1:
+            print team1, 'wins.'
+            return 1
+        elif winner == rname2:
+            print team2, 'wins.'
+            return 2
         else:
-            tmp = lastline.split("'")
-            print tmp
-            # FIXME: the names in the output do *not* match the names in the participants file!
-            winner = tmp[1]
-            loser = tmp[3]
-            if winner == team1:
-                print team1, 'wins.'
-                return 1
-            elif winner == team2:
-                print team2, 'wins.'
-                return 2
-            else:
-                print "Unable to parse winning result :("
-                return 0
+            print "Unable to parse winning result :("
+            return 0
 
 
 def start_deathmatch(team1, team2):
@@ -98,9 +102,12 @@ def pp_round1_results(teams, points):
 
 def round1(teams):
     """Run the first round and return a sorted list of team names."""
+    print
+    print "ROUND 1 (Everybody vs Everybody)"
+    print '================================'
+    print
     points = [0 for i in range(len(teams))]
     round1 = organize_first_round(len(teams))
-    print "ROUND 1 (Everybody vs Everybody)"
     for t1, t2 in round1:
         winner = start_match(teams[t1], teams[t2])
         if winner == 0:
@@ -139,7 +146,10 @@ def pp_round2_results(teams, w1, w2, w3, w4):
 
 def round2(teams):
     """Run the second round and return the name of the winning team."""
+    print
     print 'ROUND 2 (K.O.)'
+    print '=============='
+    print
     w1, w2, w3, w4 = "???", "???", "???", "???"
     # 1 vs 4
     w1 = start_deathmatch(teams[0], teams[3])
@@ -157,7 +167,7 @@ def round2(teams):
 
 
 if __name__ == '__main__':
-    teams = get_teams()
+    teams = ['group0', 'group1', 'group2', 'group3', 'group4']
     result = round1(teams)
     winner = round2(result)
 
