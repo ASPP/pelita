b1ca90b5f65206e9a7f469ce20bb48e7fd8f66df
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2011-09-13 13:22:12 +0100

    ENH: do sanity checks on imported modules.
    Comes with tests.

--- pelita/utils/__init__.py
+++ pelita/utils/__init__.py
@@ -1,7 +1,16 @@
 # -*- coding: utf-8 -*-
 
+import sys
+import contextlib as _contextlib
 from .threading_helpers import *
 from .debug import *
 
 __docformat__ = "restructuredtext"
 
+@_contextlib.contextmanager
+def with_sys_path(dirname):
+    sys.path.insert(0, dirname)
+    try:
+        yield
+    finally:
+        sys.path.remove(dirname)
