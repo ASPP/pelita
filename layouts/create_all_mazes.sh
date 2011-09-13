02522ea32219999e98f722a5ff66e94b2ea4643c
Pietro Berkes <pietro.berkes@googlemail.com>
2011-09-13 11:53:30 +0100

    FIX: Fix layout naming convention

--- layouts/create_all_mazes.sh
+++ layouts/create_all_mazes.sh
@@ -7,22 +7,22 @@ for COUNT in $(seq -w 1 $N);
 do
    SEED=$(expr $SEED + 27)
    echo normal $COUNT
-   ./create_maze.py --seed=$SEED --no-dead-ends --height=16 --width=32 > normal_$COUNT.layout
-   ./create_maze.py --seed=$SEED --dead-ends --height=16 --width=32 > "normal_dead_ends_"$COUNT".layout"
+   ./create_maze.py --seed=$SEED --no-dead-ends --height=16 --width=32 > "normal_without_dead_ends_"$COUNT".layout"
+   ./create_maze.py --seed=$SEED --dead-ends --height=16 --width=32 > "normal_with_dead_ends_"$COUNT".layout"
 done
 
 for COUNT in $(seq -w 1 $N);
 do
    SEED=$(expr $SEED + 27)
    echo small $COUNT
-   ./create_maze.py --seed=$SEED --no-dead-ends --food=10 --height=8 --width=18 > small_$COUNT.layout
-   ./create_maze.py --seed=$SEED --dead-ends --food=10 --height=8 --width=18 > "small_dead_ends_"$COUNT".layout"
+   ./create_maze.py --seed=$SEED --no-dead-ends --food=10 --height=8 --width=18 > "small_without_dead_ends_"$COUNT".layout"
+   ./create_maze.py --seed=$SEED --dead-ends --food=10 --height=8 --width=18 > "small_with_dead_ends_"$COUNT".layout"
 done
 
 for COUNT in $(seq -w 1 $N);
 do
    SEED=$(expr $SEED + 27)
    echo big $COUNT
-   ./create_maze.py --seed=$SEED --no-dead-ends --food=60 --height=32 --width=64 > big_$COUNT.layout
-   ./create_maze.py --seed=$SEED --dead-ends --food=60 --height=32 --width=64 > "big_dead_ends_"$COUNT".layout"
+   ./create_maze.py --seed=$SEED --no-dead-ends --food=60 --height=32 --width=64 > "big_without_dead_ends_"$COUNT".layout"
+   ./create_maze.py --seed=$SEED --dead-ends --food=60 --height=32 --width=64 > "big_with_dead_ends_"$COUNT".layout"
 done
