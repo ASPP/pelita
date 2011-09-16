370a0a59140612726752442164dc21a76031f7ae
Valentin Haenel <valentin.haenel@gmx.de>
2011-09-16 15:47:59 +0100

    DOC: enforce sphinx 1.1

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -21,6 +21,8 @@ import pelita
 
 # -- General configuration -----------------------------------------------------
 
+needs_sphinx='1.1'
+
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 sys.path.append('sphinxext')
