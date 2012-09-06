2757cef187708963061d29301121f1fc4b2f926c
Tiziano Zito <opossumnano@gmail.com>
2012-09-06 09:46:48 +0200

    RF: refactoring and clean up of tournament script

--- tournament/tournament.py
+++ tournament/tournament.py
@@ -1,125 +1,70 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 from __future__ import print_function
-import sys, os
-import json
-
-try:
-    import argparse
-except ImportError:
-    from pelita.compat import argparse
-
-
-parser = argparse.ArgumentParser(description='Run a tournament',
-                                 add_help=False,
-                                 formatter_class=argparse.RawDescriptionHelpFormatter)
-prog = parser.prog
-parser._positionals = parser.add_argument_group('Arguments')
-parser.add_argument('pelitagame', help='The pelitagame script')
-
-parser._optionals = parser.add_argument_group('Options')
-parser.add_argument('--help', '-h', help='show this help message and exit',
-                    action='store_const', const=True)
-
-parser.add_argument('--teams', help='load teams from TEAMFILE',
-                    metavar="TEAMFILE.json", default="teams.json")
-
-parser.epilog = """
-TEAMFILE.json must be of the form:
-    { "group0": ["Name0", "Name1", "Name2"],
-      "group1": ["Name0", "Name1", "Name2"],
-      "group2": ["Name0", "Name1", "Name2"],
-      "group3": ["Name0", "Name1", "Name2"],
-      "group4": ["Name0", "Name1", "Name2"]
-    }
-"""
-
-args = parser.parse_args()
-if args.help:
-    parser.print_help()
-    sys.exit(0)
-
-PELITA = args.pelitagame
-if not os.path.exists(PELITA) or not os.path.isfile(PELITA):
-    sys.stderr.write(PELITA+' not found!\n')
-    sys.exit(2)
-
-# FIXME: fit that for tournament
-CMD_STUB = PELITA+' --rounds=300 --tk'
-#CMD_STUB = PELITA+' --rounds=10 --null'
-SPEAK = '/usr/bin/flite'
-
-# the 'real' names of the teams (instead of group0 .. group4). they are
-# collected while the tournament goes
-rnames = {'group0' : 'group0',
-          'group1' : 'group1',
-          'group2' : 'group2',
-          'group3' : 'group3',
-          'group4' : 'group4' }
-
-with open(args.teams) as teamfile:
-    group_members = json.load(teamfile)
-
 from subprocess import Popen, PIPE, STDOUT, check_call
 import random
 import time
 import tempfile
 import cStringIO
+import sys
+import os
+import json
+# Drop this as soon as we drop support for python 2.6
+try:
+    import argparse
+except ImportError:
+    from pelita.compat import argparse
 
-# check for festival support
-if not os.path.exists(SPEAK.split()[0]):
-    SPEAK=False
+# Speaking external program.
+# Probably not worth it to make it an option.
+FLITE = '/usr/bin/flite'
 
-random.seed(42) # -> guaranteed to be random
+# Global random seed. Keep it fixed or it may be impossible
+# to replicate a tournament.
+# Individual matches will get a random seed derived from this.
+random.seed(42)
 
 # Number of points a teams gets for matches in the first round
+# Probably not worth it to make it options.
 POINTS_DRAW = 1
 POINTS_WIN = 2
 
-def get_seed():
-    return random.randint(0, sys.maxint)
-
 def print(*args, **kwargs):
     """Speak while you print. To disable set speak=False.
-    You need the program 'flite' to be able to speak.
-    Set wait=X to wait X seconds after speaking."""
+    You need the program %s to be able to speak.
+    Set wait=X to wait X seconds after speaking."""%FLITE
     if len(args) == 0:
         __builtins__.print()
         return
     stream = cStringIO.StringIO()
     wait = kwargs.pop('wait', 0.5)
     want_speak = kwargs.pop('speak', SPEAK)
-    if want_speak:
+    if not want_speak:
+        __builtins__.print(*args, **kwargs)
+    else:
         __builtins__.print(*args, file=stream, **kwargs)
         string = stream.getvalue()
         __builtins__.print(string, end='')
         sys.stdout.flush()
-        speak(string, wait=wait)
-    else:
-        __builtins__.print(*args, **kwargs)
-    
-def speak(string, wait=0.5):
-    with tempfile.NamedTemporaryFile() as text:
-        text.write(string+'\n')
-        text.flush()
-        festival = check_call(SPEAK.split()+[text.name])
-    time.sleep(wait)
+        with tempfile.NamedTemporaryFile() as text:
+            text.write(string+'\n')
+            text.flush()
+            festival = check_call(SPEAK.split()+[text.name])
+        time.sleep(wait)
 
 def present_teams():
-    print('Hello master, I am the Python drone. I am here to serve you.', 
-          wait=1.5)
+    print('Hello master, I am the Python drone. I am here to serve you.', wait=1.5)
     print('Welcome to the Pelita tournament', wait=1.5)
     print('This evening the teams are:', wait=1.5)
-    for group in sorted(rnames.keys()):
-        print(group, rnames[group])
+    for group in sorted(RNAMES.keys()):
+        print(group, RNAMES[group])
         [print(member, wait=0.1) for member in group_members[group]]
         time.sleep(1)
-        print('This was', group, rnames[group],wait=1.5)
     print('These were the teams. Now you ready for the fight?')
-    
+
 def set_name(team):
     """Get name of team using a dry-run pelita game"""
-    global rnames
+    global RNAMES
     args = CMD_STUB.split()
     args.extend(['--dry-run', team, 'random'])
     stdout, stderr = Popen(args, stdout=PIPE, stderr=PIPE).communicate()
@@ -127,10 +72,10 @@ def set_name(team):
         if line.startswith("Using factory '"):
             split = line.split("'")
             tname, rname = split[1], split[3]
-            if tname in rnames:
-                rnames[tname] = rname
+            if tname in RNAMES:
+                RNAMES[tname] = rname
     if stderr != '':
-        print("*** ERROR: I could not load team", team, ". Please help!", 
+        print("*** ERROR: I could not load team", team, ". Please help!",
               speak=False)
         print(stderr, speak=False)
         sys.exit(1)
@@ -139,14 +84,14 @@ def start_match(team1, team2):
     """Start a match between team1 and team2. Return which team won (1 or 2) or
     0 if there was a draw.
     """
-    global rnames
+    global RNAMES
     print()
-    print('Starting match: '+ rnames[team1]+' vs ' + rnames[team2])
+    print('Starting match: '+ RNAMES[team1]+' vs ' + RNAMES[team2])
     print()
-    raw_input('--- Press ENTER to continue ---\n')
+    #raw_input('--- Press ENTER to continue ---\n')
     args = CMD_STUB.split()
-    dumpfile = 'dumpstore/'+time.strftime('%Y%m%d-%H%M%S') 
-    args.extend([team1, team2, '--dump', dumpfile,'--seed', str(get_seed())])
+    dumpfile = 'dumpstore/'+time.strftime('%Y%m%d-%H%M%S')
+    args.extend([team1, team2, '--dump', dumpfile,'--seed', str(random.randint(0, sys.maxint))])
     stdout, stderr = Popen(args, stdout=PIPE, stderr=PIPE).communicate()
     tmp = reversed(stdout.splitlines())
     lastline = None
@@ -169,11 +114,11 @@ def start_match(team1, team2):
         tmp = lastline.split("'")
         winner = tmp[1]
         loser = tmp[3]
-        if winner == rnames[team1]:
-            print(rnames[team1], 'wins.')
+        if winner == RNAMES[team1]:
+            print(RNAMES[team1], 'wins.')
             return 1
-        elif winner == rnames[team2]:
-            print(rnames[team2], 'wins.')
+        elif winner == RNAMES[team2]:
+            print(RNAMES[team2], 'wins.')
             return 2
         else:
             print("Unable to parse winning result :(")
@@ -183,9 +128,6 @@ def start_deathmatch(team1, team2):
     """Start a match between team1 and team2 until one of them wins (ie no
     draw.)
     """
-    # FIXME: What if there is *always* a draw? We're in K.O. mode so we cannot
-    # let both teams proceed.
-    # do at most 3 death matches:
     for i in range(3):
         r = start_match(team1, team2)
         if r == 0:
@@ -194,7 +136,7 @@ def start_deathmatch(team1, team2):
         winner = team1 if r == 1 else team2
         return winner
     # if we are here, we have no winner after 3 death matches
-    # just asisgn a random winner
+    # just assign a random winner
     print('No winner after 3 Death Matches. Choose a winner at random:', wait=2)
     winner = random.choice((team1, team2))
     print('And the winner is', winner)
@@ -202,11 +144,11 @@ def start_deathmatch(team1, team2):
 
 def pp_round1_results(teams, points):
     """Pretty print the current result of the matches."""
-    global rnames
+    global RNAMES
     result = sorted(zip(points, teams), reverse=True)
     print('Current Ranking:')
     for p, t in result:
-        print("  %25s %d" % (rnames[t], p))
+        print("  %25s %d" % (RNAMES[t], p))
     print()
 
 
@@ -228,7 +170,6 @@ def round1(teams):
             ij = [i, j]
             random.shuffle(ij)
             round1.append(ij)
-    #round1 = [[i, j] for i in range(5) for j in range(i+1, 5)]
     # shuffle the matches for more fun
     random.shuffle(round1)
     for t1, t2 in round1:
@@ -239,7 +180,7 @@ def round1(teams):
         else:
             points[[t1, t2][winner-1]] += POINTS_WIN
         pp_round1_results(teams, points)
-    
+
     # Sort the teams by points and return the team names as a list
     result = sorted(zip(points, teams), reverse=True)
     result = [t for p, t in result]
@@ -252,20 +193,20 @@ def pp_round2_results(teams, w1, w2, w3, w4):
     teams is the list [group0, group1, ...] not the names of the agens, sorted
     by the result of the first round.
     """
-    names = dict(rnames)
+    names = dict(RNAMES)
     names['???'] = '???'
-    feed = max(len(item) for item in rnames.values())+2
+    feed = max(len(item) for item in RNAMES.values())+2
     lengths={}
     for name in names:
         lengths[name] = feed - len(names[name])
 
-    semifinal_top_up = names[teams[0]]+' '+"─"*lengths[teams[0]]+'┐' 
-    final_top = " "*feed+' ├─ '+names[w1]+' '+'─'*lengths[w1]+'┐' 
-    semifinal_top_down = names[teams[3]]+' '+"─"*lengths[teams[3]]+'┘' 
+    semifinal_top_up = names[teams[0]]+' '+"─"*lengths[teams[0]]+'┐'
+    final_top = " "*feed+' ├─ '+names[w1]+' '+'─'*lengths[w1]+'┐'
+    semifinal_top_down = names[teams[3]]+' '+"─"*lengths[teams[3]]+'┘'
     preliminary_winner = (" "*(2*feed+5)+'├─ '+names[w3]+' '+
                           '─'*lengths[w3]+'┐ ')
     semifinal_bottom_up = names[teams[1]]+' '+"─"*lengths[teams[1]]+'┐'
-    final_bottom = " "*feed+' ├─ '+names[w2]+' '+'─'*lengths[w2]+'┘' 
+    final_bottom = " "*feed+' ├─ '+names[w2]+' '+'─'*lengths[w2]+'┘'
     semifinal_bottom_down = names[teams[2]]+' '+"─"*lengths[teams[2]]+'┘'
     looser = names[teams[4]]+' '+"─"*lengths[teams[4]]+'─'*(2*feed+8)+'┘'
     winner = " "*(3*feed+9)+'├─ '+names[w4]
@@ -313,10 +254,74 @@ def round2(teams):
     return w4
 
 if __name__ == '__main__':
+    # Command line argument parsing.
+    # Oh, why must argparse be soo verbose :(
+    parser = argparse.ArgumentParser(description='Run a tournament',
+                                 add_help=False,
+                                 formatter_class=argparse.RawDescriptionHelpFormatter)
+
+    parser._positionals = parser.add_argument_group('Arguments')
+    parser.add_argument('pelitagame', help='The pelitagame script')
+
+    parser._optionals = parser.add_argument_group('Options')
+    parser.add_argument('--help', '-h', help='show this help message and exit',
+                        action='store_const', const=True)
+    parser.add_argument('--speak', '-s', help='speak loudly every messsage on stdout',
+                        action='store_const', const=True)
+    parser.add_argument('--rounds', '-r', help='maximum number of rounds to play per match',
+                                            type=int, default=300)
+    parser.add_argument('--viewer', '-v', help='the pelita viewer to use',
+                                                                default='tk')
+    parser.add_argument('--teams', help='load teams from TEAMFILE',
+                    metavar="TEAMFILE.json", default="teams.json")
+
+    parser.epilog = """
+TEAMFILE.json must be of the form:
+    { "group0": ["Name0", "Name1", "Name2"],
+      "group1": ["Name0", "Name1", "Name2"],
+      "group2": ["Name0", "Name1", "Name2"],
+      "group3": ["Name0", "Name1", "Name2"],
+      "group4": ["Name0", "Name1", "Name2"]
+    }
+"""
+
+    args = parser.parse_args()
+
+    if args.help:
+        parser.print_help()
+        sys.exit(0)
+
+    # Check that pelitagame can be run
+    if not os.path.exists(args.pelitagame) or not os.path.isfile(args.pelitagame):
+        sys.stderr.write(args.pelitagame+' not found!\n')
+        sys.exit(2)
+    else:
+        # Define the command line to run a pelita match
+        CMD_STUB = args.pelitagame+' --rounds=%d'%args.rounds+' --%s'%args.viewer
+
+    SPEAK = False
+    if args.speak:
+        # check for speaking support
+        if os.path.exists(FLITE):
+            SPEAK = True
+
+    # the 'real' names of the teams (instead of group0 .. group4). they are
+    # collected while the tournament goes
+    RNAMES = {'group0' : 'group0',
+              'group1' : 'group1',
+              'group2' : 'group2',
+              'group3' : 'group3',
+              'group4' : 'group4' }
+
+    with open(args.teams) as teamfile:
+        group_members = json.load(teamfile)
+
+
     # create a directory for the dumps
     if not os.path.exists('dumpstore'):
         os.mkdir('dumpstore')
-    teams = rnames.keys()
+
+    teams = RNAMES.keys()
     random.shuffle(teams)
     # load team names
     for team in teams:
@@ -325,5 +330,5 @@ if __name__ == '__main__':
     result = round1(teams)
     winner = round2(result)
     print('The winner of the St Andrews Pelita tournament is...', wait=2)
-    print(rnames[winner], '. Congratulations!', wait=2)
+    print(RNAMES[winner], '. Congratulations!', wait=2)
     print('Good evening master. It was a pleasure to serve you.')
