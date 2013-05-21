c65f1d9a84850a8899f171f29c74a3158cf7697b
Rike-Benjamin Schuppner <rikebs@debilski.de>
2013-05-21 16:01:29 +0200

    UT: Test universe.reachable

--- test/test_datmodel.py
+++ test/test_datmodel.py
@@ -599,6 +599,19 @@ class TestCTFUniverse(unittest.TestCase):
         universe.teams.append(Team(2, "noname", ()))
         self.assertRaises(UniverseException, universe.enemy_team, 0)
 
+    def test_reachable(self):
+        test_layout = (
+        """ ##################
+            #0#.  .  # .1#   #
+            #2#####    ##### #
+            #     . #  .  .#3#
+            ################## """)
+        universe = create_CTFUniverse(test_layout, 4)
+        reachable = [universe.reachable([bot.initial_pos]) for bot in universe.bots]
+        self.assertTrue(bot.initial_pos in reachable[i] for i, bot in enumerate(universe.bots))
+        self.assertTrue(universe.bots[0].initial_pos in dict(reachable[1]))
+        self.assertFalse(universe.bots[3].initial_pos in dict(reachable[0]))
+        self.assertFalse(universe.bots[0].initial_pos in dict(reachable[3]))
 
 class TestCTFUniverseRules(unittest.TestCase):
 
