7603cdbc9f96a7e3dbf14d6bcf8d2868175fed27
Rike-Benjamin Schuppner <rikebs@debilski.de>
2015-07-30 00:31:18 +0200

    ENH: Creating mazes with Python 3.

--- layouts/create_pelita_layouts.py
+++ layouts/create_pelita_layouts.py
@@ -1,7 +1,9 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # Use this script to update/regenerate the layouts strings in pelita.layouts.py
 
 import os
+import zlib
+import base64
 import pelita
 
 EXTENSION = '.layout'
@@ -17,14 +19,13 @@ for f in sorted(os.listdir(local_dir)):
     flname, ext = os.path.splitext(f)
     if ext != EXTENSION:
         continue
-    with open(os.path.join(local_dir,f), 'r') as text:
-        layout = text.read()
+    with open(os.path.join(local_dir,f), 'rb') as bytemaze:
+        layout = bytemaze.read()
         # create string variable
-        content += 'layout_' + flname
+        content += 'layout_' + flname + ' = '
         # encode layout string
-        content += ' = """' + layout.encode('zlib').encode('base64') + '"""\n'
+        content += '"""' + base64.encodebytes(zlib.compress(layout)).decode() + '"""\n'
 
 # write out file in pelita directory
 with open(outfile, 'w') as out:
     out.write(content)
-
