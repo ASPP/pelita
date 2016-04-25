8cb8e27e8cd5b98d36e1b98ddb0cb187da30a60b
Rike-Benjamin Schuppner <rikebs@debilski.de>
2016-04-25 23:24:48 +0200

    DOC/BF: Fix the missing link to the GitHub repo.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -128,7 +128,10 @@ html_theme = 'alabaster'
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-#html_theme_options = {}
+html_theme_options = {
+    'github_user': 'ASPP',
+    'github_repo': 'pelita',
+}
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
