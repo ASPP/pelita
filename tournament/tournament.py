ae56c16ad4b09f6516ae4edf999c61e3f8ee0469
Rike-Benjamin Schuppner <rikebs@debilski.de>
2016-08-15 16:48:58 +0200

    ENH: tournament.py --setup

--- tournament/tournament.py
+++ tournament/tournament.py
@@ -1,13 +1,18 @@
 #!/usr/bin/env python3
 
 import argparse
+import datetime
 import itertools
 import os
 import random
+import re
+import shlex
 import sys
 
+import shutil
 import yaml
 
+from pelita import libpelita
 from tournament import tournament
 from tournament.tournament import Config, State
 
@@ -30,6 +35,97 @@ def create_directory(prefix):
         else:
             break
     return name
+
+def input_choice(text, choices, vars):
+    selected = ""
+    while not selected or selected not in vars:
+        print(text)
+        for choice in choices:
+            print(choice)
+        selected = input().strip()
+    return selected
+
+
+def autoconf_sound():
+    res = {}
+    which_say = shutil.which("say")
+    if which_say:
+        res["say"] = [which_say, "-f"]
+    which_flite = shutil.which("flite")
+    if which_flite:
+        res["flite"] = [which_flite]
+    return res
+
+
+def setup():
+    config = {}
+    print("Where should the next tournament be?")
+    config['location'] = input()
+    print("When should the next tournament be? (ex. {year})".format(year=datetime.datetime.now().year))
+    config['date'] = input()
+
+    while True:
+        sound = autoconf_sound()
+        sound_options = [
+            "(e)nter manually",
+        ]
+        keys = "e"
+
+        try:
+            which_say = sound["say"]
+            sound_options.append("(s)ay ({})".format(sound["say"]))
+            keys += "s"
+        except KeyError:
+            pass
+
+        try:
+            which_say = sound["flite"]
+            sound_options.append("(f)flite ({})".format(sound["flite"]))
+            keys += "f"
+        except KeyError:
+            pass
+
+        sound_options.append("(n)o sound")
+        keys += "n"
+        sound_options.append("(r)etry search")
+        keys += "r"
+
+        res = input_choice("Enable sound?", sound_options, keys)
+        if res == "n":
+            config["speak"] = False
+            break
+        elif res == "e":
+            print("Please enter the location of the sound-giving binary:")
+            sound_path = input()
+        elif res == "s":
+            sound_path = libpelita.shlex_unsplit(sound["say"])
+        elif res == "f":
+            sound_path = libpelita.shlex_unsplit(sound["flite"])
+        else:
+            continue
+
+        print("Now trying to speak:")
+        config["speak"] = True
+        config['speaker'] = sound_path
+        # must set a few dummy variables
+        config['teams'] = []
+        config['bonusmatch'] = []
+        Config(config).say("Hello my master.")
+        success = input_choice("Did you hear any sound? (y/n)", [], "yn")
+        if success == "y":
+            del config["teams"]
+            del config["bonusmatch"]
+            break
+
+    def escape(str):
+        return "-" + re.sub(r'[\W]', '_', str) if str else ""
+
+    file_name = "tournament{location}{year}.yaml".format(location=escape(config["location"]),
+                                                         year=escape(config["date"]))
+
+    print("Writing to: {file_name}".format(file_name=file_name))
+    with open(file_name, "x") as f:
+        yaml.dump(config, f, default_flow_style=False)
 
 
 if __name__ == '__main__':
@@ -56,8 +152,15 @@ if __name__ == '__main__':
                         type=str, help='the pelita viewer to use (default: tk)')
     parser.add_argument('--config', help='tournament data',
                         metavar="CONFIG_YAML", default="tournament.yaml")
-    parser.add_argument('--non-interactive', help='do not ask before proceeding',
-                        action='store_true')
+
+    interactivity = parser.add_mutually_exclusive_group()
+    interactivity.add_argument('--non-interactive', help='do not ask before proceeding',
+                                dest='interactive', action='store_const', const=False)
+    interactivity.add_argument('--interactive', help='ask before proceeding',
+                               dest='interactive', action='store_const', const=True)
+
+    parser.add_argument('--setup', action='store_true')
+
     parser.add_argument('--state', help='store state',
                         metavar="STATEFILE", default='state.yaml')
     parser.add_argument('--load-state', help='load state from file',
@@ -71,6 +174,9 @@ if __name__ == '__main__':
     ARGS = parser.parse_args()
     if ARGS.help:
         parser.print_help()
+        sys.exit(0)
+    elif ARGS.setup:
+        setup()
         sys.exit(0)
 
 
@@ -88,10 +194,16 @@ if __name__ == '__main__':
         fd = os.open(logfile, os.O_CREAT|os.O_EXCL|os.O_WRONLY, 0o0666)
         LOGFILE = os.fdopen(fd, 'w')
 
+    def firstNN(*args):
+        """
+        Return the first argument not None.
+        """
+        return next(filter(None, args), None)
+
     with open(ARGS.config) as f:
         config_data = yaml.load(f)
         config_data['viewer'] = ARGS.viewer or config_data.get('viewer', 'tk')
-        config_data['interactive'] = not ARGS.non_interactive
+        config_data['interactive'] = firstNN(ARGS.viewer, config_data.get('interactive'), 'True')
         config_data['statefile'] = ARGS.state
         config_data['speak'] = ARGS.speak
         config_data['speaker'] = ARGS.speaker
