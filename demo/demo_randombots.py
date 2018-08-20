50da75214b22c12d25cc923586e5d87685a425b4
Tiziano Zito <opossumnano@gmail.com>
2018-08-20 15:13:23 +0200

    get rid of obsolete demos

--- demo/demo_randombots.py
+++
@@ -1,5 +0,0 @@
-TEAM_NAME = 'RandomBots'
-
-def move(turn, game):
-    bot = game.team[turn]
-    return bot.random.choice(bot.legal_moves)
--- demo/demo_stoppingbots.py
+++
@@ -1,5 +0,0 @@
-TEAM_NAME = 'StoppingBots'
-
-def move(turn, game):
-    # do not move at all
-    return (0,0)
