522dc1b371e742ff298a22250a24b448a2995c0e
Tiziano Zito <opossumnano@gmail.com>
2018-08-22 11:10:28 +0200

    improve docs of test_smartrandom

--- template/test_demo_smartrandom.py
+++ template/test_demo_smartrandom.py
@@ -2,20 +2,21 @@ from demo_smartrandom import move
 from pelita.utils import setup_test_game
 
 def test_legalmoves():
+    # check that the only valid move is always returned
+    # we try ten times, to test 10 different random streams
     layout="""
     ########
     #0######
     #1. .EE#
     ########
     """
-    # check that the only valid move is always returned
-    # we try ten times, to test 10 different random streams
     for i in range(10):
         game = setup_test_game(layout=layout, is_blue=True)
         next_move = move(0, game)
         assert next_move == (0,1)
 
 def test_eat_enemy():
+    # check that we indeed it a enemy when possible
     layout="""
     ########
     #E###.##
@@ -27,6 +28,7 @@ def test_eat_enemy():
     assert next_move == (0,-1)
 
 def test_eat_food():
+    # check that we indeed collect food when possible
     layout="""
     ########
     #E # .##
@@ -37,18 +39,8 @@ def test_eat_food():
     next_move = move(0, game)
     assert next_move == (0,-1)
 
-def test_no_kamikaze_stop():
-    layout="""
-    ########
-    #  ###.#
-    #1. E0E#
-    ########
-    """
-    game = setup_test_game(layout=layout, is_blue=True)
-    next_move = move(0, game)
-    assert next_move == (0, 0)
-
 def test_no_kamikaze_back():
+    # check that we escape if faced with an enemy
     layout="""
     ########
     #E ###.#
@@ -59,3 +51,14 @@ def test_no_kamikaze_back():
     next_move = move(0, game)
     assert next_move == (-1, 0)
 
+def test_no_kamikaze_stop():
+    # Check that we stop if escaping would kill us
+    layout="""
+    ########
+    #  ###.#
+    #1. E0E#
+    ########
+    """
+    game = setup_test_game(layout=layout, is_blue=True)
+    next_move = move(0, game)
+    assert next_move == (0, 0)
