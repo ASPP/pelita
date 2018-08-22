16a5b95282e10200f8273182a1f4044347dd9627
Tiziano Zito <opossumnano@gmail.com>
2018-08-22 10:54:12 +0200

    fix test_demo_stopping given the last create_layout interface

--- template/test_demo_stopping.py
+++ template/test_demo_stopping.py
@@ -2,19 +2,22 @@ from demo_stopping import move
 from pelita.utils import setup_test_game, create_layout
 
 def test_stays_there():
+    # Given a simple layout, verify that the bot does not move, indipendent
+    # of its initial position.
     layout="""
     ########
     #     .#
     #.1  EE#
     ########
     """
-    # verify that we always stay where we are, indipendent
-    # of that initial position. Try all possible initial
-    # positions
-    all_locations = ((x, y) for x in range(1,7) for y in range(1,7))
+    # generate all possible locations within the maze
+    all_locations = ((x, y) for x in range(8) for y in range(4))
     for loc in all_locations:
-        loc_layout = create_layout(layout)
-        loc_layout.bot_positions["0"] = loc
+        try:
+            loc_layout = create_layout(layout, bots=[loc, None])
+        except ValueError:
+            # loc is a wall, skip this position
+            continue
         game = setup_test_game(layout=loc_layout, is_blue=True)
         next_move = move(0, game)
         assert next_move == (0,0)
