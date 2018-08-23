32a29a5de681b497a3d44e000e8fa298e65ad0b6
Tiziano Zito <opossumnano@gmail.com>
2018-08-23 13:06:41 +0200

    handle differente game.states for bot using two incompatible strategies

--- template/demo_one_and_one.py
+++ template/demo_one_and_one.py
@@ -1,11 +1,23 @@
 # We build a team of bots, one basic defender and one basic attacker
 TEAM_NAME = 'one and one'
 
-from demo_basic_defender import move as move_defender
-from demo_basic_attacker import move as move_attacker
+from demo05_basic_defender import move as move_defender
+from demo04_basic_attacker import move as move_attacker
 
 def move(turn, game):
-    if turn == 0:
-        return move_defender(turn, game)
+    if game.state is None:
+        one_by_one_state = {'attacker' : None, 'defender' : None}
     else:
-        return move_attacker(turn, game)
+        one_by_one_state = game.state
+
+    if turn == 0:
+        game.state = one_by_one_state['defender']
+        next_move = move_defender(turn, game)
+    else:
+        game.state = one_by_one_state['attacker']
+        next_move = move_attacker(turn, game)
+
+    # reset the game state
+    game.state = one_by_one_state
+
+    return next_move
