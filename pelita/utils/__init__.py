b0899449d58b49aec3123a324fd7c49fe0641684
Tiziano Zito <opossumnano@gmail.com>
2018-08-28 12:41:18 +0200

    minimal docs for setup_test_game

--- pelita/utils/__init__.py
+++ pelita/utils/__init__.py
@@ -4,6 +4,10 @@ from ..player.team import create_layout, Game, bots_from_layout
 from ..graph import Graph
 
 def setup_test_game(*, layout, game=None, is_blue=True, round=None, score=None, seed=None):
+    """Return a game object given a layout.
+
+    The returned Game instance can be passed to a move function to test its return value.
+    The layout is a string that can be passed to create_layout."""
     if game is not None:
         raise RuntimeError("Re-using an old game is not implemented yet.")
 
@@ -17,7 +21,7 @@ def setup_test_game(*, layout, game=None, is_blue=True, round=None, score=None, 
     if score is None:
         score = [0, 0]
     bots = bots_from_layout(layout, is_blue, score, rng, round, team_name=['blue', 'red'], timeout_count=[0, 0])
-  
+
     if is_blue:
         team = [bots[0], bots[2]]
     else:
@@ -25,4 +29,4 @@ def setup_test_game(*, layout, game=None, is_blue=True, round=None, score=None, 
 
     state = None
     game = Game(team, state)
-    return game 
+    return game
