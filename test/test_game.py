1ce1a3dcca5b2d0936cd34d3368200c2318d8934
Lisa Schwetlick <lisaschwetlick@gmail.com>
2019-02-24 10:28:31 +0100

    added tests for game module

--- test/test_game.py
+++ test/test_game.py
@@ -1,7 +1,12 @@
+import glob
+import pytest
+import numpy as np
 from pelita.game import initial_positions
+from pelita.game import play_turn
 
+from pelita import layout
 
-def test_initial_positions():
+def test_initial_positions_basic():
     walls = [(0, 0),
              (0, 1),
              (0, 2),
@@ -26,6 +31,52 @@ def test_initial_positions():
              (7, 2),
              (7, 3)]
     out = initial_positions(walls)
-    exp = [(1, 2), (6, 1), (1, 1), (6, 2)]
+    exp = [(1, 1), (6, 2), (1, 2), (6, 1)]
     assert len(out) == 4
     assert out == exp
+
+@pytest.mark.parametrize('n_times', range(30))
+def test_initial_positions_same_in_layout_random(n_times):
+    seedval = np.random.randint(2**32)
+    print(f'seedval: {seedval}')
+    # for i in range(n_times):
+    l = layout.get_random_layout()
+    parsed_l = layout.parse_layout(l[1])
+    exp = parsed_l["bots"]
+    walls = parsed_l["walls"]
+    out = initial_positions(walls)
+    assert out == exp
+
+def test_initial_positions_same_in_layout():
+    layouts = glob.glob("../layouts/*")
+    for path in layouts:
+        l = layout.load_layout(layout_file=path)
+        parsed_l = layout.parse_layout(l[1])
+        exp = parsed_l["bots"]
+        walls = parsed_l["walls"]
+        out = initial_positions(walls)
+        assert out == exp
+
+
+def test_play_turn():
+    seedval = np.random.randint(2**32)
+    print(f'seedval: {seedval}')
+    l = layout.get_random_layout()
+    parsed_l = layout.parse_layout(l[1])
+    game_state = {
+                  "food": parsed_l["food"],
+                  "walls": parsed_l["walls"],
+                  "bots": parsed_l["bots"],
+                  "max_round": 300,
+                  "team_names": ("a", "b"),
+                  "turn": 1,
+                  "round": 1,
+                  "timeout": [],
+                  "gameover": False,
+                  "whowins": None,
+                  "team_say": "bla",
+                  "score": 0,
+                  "deaths": 0,
+                  }
+    game_state = play_turn(game_state, 0, (1, 2))
+    # TODO is move legal?
