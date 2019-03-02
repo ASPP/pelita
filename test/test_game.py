e8f2a8b2f0dbd62e689b752be853ed8822f4c082
Rike-Benjamin Schuppner <rikebs@debilski.de>
2019-03-02 18:38:44 +0100

    TST: Simplify test for initial positions

--- test/test_game.py
+++ test/test_game.py
@@ -9,29 +9,13 @@ from pelita import layout
 
 def test_initial_positions_basic():
     """Checks basic example for initial positions"""
-    walls = [(0, 0),
-             (0, 1),
-             (0, 2),
-             (0, 3),
-             (1, 0),
-             (1, 3),
-             (2, 0),
-             (2, 1),
-             (2, 3),
-             (3, 0),
-             (3, 1),
-             (3, 3),
-             (4, 0),
-             (4, 1),
-             (4, 3),
-             (5, 0),
-             (5, 3),
-             (6, 0),
-             (6, 3),
-             (7, 0),
-             (7, 1),
-             (7, 2),
-             (7, 3)]
+    simple_layout = """
+    ########
+    # ###  #
+    #      #
+    ########
+    """
+    walls = layout.parse_layout(simple_layout)['walls']
     out = initial_positions(walls)
     exp = [(1, 1), (6, 2), (1, 2), (6, 1)]
     assert len(out) == 4
