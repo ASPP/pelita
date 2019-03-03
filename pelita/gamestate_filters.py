168db834186a147505efd048b22027c42dff98c5
Rike-Benjamin Schuppner <rikebs@debilski.de>
2019-03-04 00:30:01 +0100

    FO: Please don’t use tabs.

--- pelita/gamestate_filters.py
+++ pelita/gamestate_filters.py
@@ -4,33 +4,34 @@ import copy
 
 ### The main function
 
+
 def noiser(gamestate, noise_radius=5, sight_distance=5, seed=None):
-	"""Function to make bot positions noisy in a game state.
+    """Function to make bot positions noisy in a game state.
 
     Applies uniform noise in maze space. Noise will only be applied if the enemy bot
-	is farther away than a certain threshold (`sight_distance`), which is
-	Manhattan distance disregarding walls.  A bot with distance of 1 in Manhattan space
+    is farther away than a certain threshold (`sight_distance`), which is
+    Manhattan distance disregarding walls.  A bot with distance of 1 in Manhattan space
     could still be much further away in maze distance.
-	
-	Distance to enemies measured in Manhattan space, disregarding walls. So, a bot
-	distance of 1 in Manhattan space could still be much further away in maze
-	distance.
-	
-	Given a `bot_index` (now 'turn' in gamestate) the method looks up the enemies
-	of this bot. It then adds uniform noise in maze space to the enemy positions, but
-	only if bot is farther away than sight_distance
-	
-	If a position is noisy or not is indicated by the `noisy` attribute in the 
-	gamestate dictionary, and then also in the returned noisy game state
+
+    Distance to enemies measured in Manhattan space, disregarding walls. So, a bot
+    distance of 1 in Manhattan space could still be much further away in maze
+    distance.
+
+    Given a `bot_index` (now 'turn' in gamestate) the method looks up the enemies
+    of this bot. It then adds uniform noise in maze space to the enemy positions, but
+    only if bot is farther away than sight_distance
+
+    If a position is noisy or not is indicated by the `noisy` attribute in the
+    gamestate dictionary, and then also in the returned noisy game state
 
     Functions needed
     ----------------
 
     altered_pos(bot_pos):
         return the noised new position of an enemy bot.
-		
-	manhattan_dist(a,b):
-		returns a scalar
+
+    manhattan_dist(a,b):
+        returns a scalar
 
     Parameters
     ----------
@@ -41,106 +42,115 @@ def noiser(gamestate, noise_radius=5, sight_distance=5, seed=None):
         the distance at which noise is no longer applied.
     seed : int, optional
         seed which initialises the internal random number generator
-		to make games replicable
-		
-	Returns
-	-------
-	noisy_gamestate : gamestate with noisy enemy positions
-		
+        to make games replicable
+
+    Returns
+    -------
+    noisy_gamestate : gamestate with noisy enemy positions
+
     """
 
-	# set the random state
-	rnd = random.Random(seed)
-	
-	# maka a new game state
-	#cp_gs = copy.deepcopy(gamestate)
-	# using a shallow copy
-	cp_gs = {}
-	cp_gs.update(gamestate)
-	
-	# get the current turn (ie the bot_index)
-	turn = cp_gs["turn"]
-	
-	# get the walls
-	walls = cp_gs["walls"]
-	
-	# get the current bot
-	# [:] makes a copy
-	bots        = cp_gs["bots"][:]
-	current_bot = bots[turn]
-	
-	# get the enemy bots
-	enemy_bots  = list(range(0,4))
-	if turn % 2:
-		# current bot is in the uneven team
-		enemy_slice = slice(0,4,2)
-	else:
-		# current bot is in the even team
-		enemy_slice = slice(1,4,2)
-	enemy_bots = bots[enemy_slice]
-	
-	# get the noisy information
-	noisy = cp_gs["noisy"]
+    # set the random state
+    rnd = random.Random(seed)
 
-	for count, b in enumerate(enemy_bots):
-		# Check that the distance between this bot and the enemy is larger
-		# than `sight_distance`.
-		cur_distance = manhattan_dist(current_bot, b)
+    # maka a new game state
+    # cp_gs = copy.deepcopy(gamestate)
+    # using a shallow copy
+    cp_gs = {}
+    cp_gs.update(gamestate)
 
-		if cur_distance is None or cur_distance > sight_distance:
-			# If so then alter the position of the enemy
-			cur_altered_pos = alter_pos(b,noise_radius,rnd,walls)
-			enemy_bots[count] = cur_altered_pos[0]
-			noisy[count]      = cur_altered_pos[1]
-	
-	# packing before return
-	bots[enemy_slice] = enemy_bots
-	cp_gs["noisy"] = noisy
-	cp_gs["bots"] = bots
-	
-	# return
-	return cp_gs
-	
-	
+    # get the current turn (ie the bot_index)
+    turn = cp_gs["turn"]
+
+    # get the walls
+    walls = cp_gs["walls"]
+
+    # get the current bot
+    # [:] makes a copy
+    bots = cp_gs["bots"][:]
+    current_bot = bots[turn]
+
+    # get the enemy bots
+    enemy_bots = list(range(0, 4))
+    if turn % 2:
+        # current bot is in the uneven team
+        enemy_slice = slice(0, 4, 2)
+    else:
+        # current bot is in the even team
+        enemy_slice = slice(1, 4, 2)
+    enemy_bots = bots[enemy_slice]
+
+    # get the noisy information
+    noisy = cp_gs["noisy"]
+
+    for count, b in enumerate(enemy_bots):
+        # Check that the distance between this bot and the enemy is larger
+        # than `sight_distance`.
+        cur_distance = manhattan_dist(current_bot, b)
+
+        if cur_distance is None or cur_distance > sight_distance:
+            # If so then alter the position of the enemy
+            cur_altered_pos = alter_pos(b, noise_radius, rnd, walls)
+            enemy_bots[count] = cur_altered_pos[0]
+            noisy[count] = cur_altered_pos[1]
+
+            # packing before return
+    bots[enemy_slice] = enemy_bots
+    cp_gs["noisy"] = noisy
+    cp_gs["bots"] = bots
+
+    # return
+    return cp_gs
+
+
 ### The subfunctions
 
-def alter_pos(bot_pos,noise_radius,rnd,walls):
-	""" alter the position """
-	
-	# extracting from walls the maximum width and height
-	max_walls = max(walls)
-	min_walls = min(walls)
 
-	# get a list of possible positions
-	x_min, x_max = bot_pos[0] - noise_radius, bot_pos[0] + noise_radius
-	y_min, y_max = bot_pos[1] - noise_radius, bot_pos[1] + noise_radius
-	
-	# filter them so the we return no positions outsided the maze
-	if x_min < 0: x_min = 1
-	if x_max > max_walls[0]: x_max = max_walls[0]
-	if y_min < 0: y_min = 1
-	if y_max > max_walls[1]: y_max = max_walls[1]
-	
-	possible_positions = [(i,j) for i in range(x_min, x_max)
-								for j in range(y_min, y_max)
-						  if manhattan_dist((i,j), bot_pos) <= noise_radius]
-	
-	# shuffle the list of positions
-	rnd.shuffle(possible_positions)
-	final_pos = bot_pos
-	noisy     = False
-	for pos in possible_positions:
-		# check that the bot won't returned as positioned on a wall square
-		if pos in walls:
-			continue
-		else:
-			final_pos = pos
-			noisy     = True
-			break
-	# return the final_pos and a flag if it is noisy or not
-	return [final_pos, noisy]
-	#return [(0,0), True]
-	
+def alter_pos(bot_pos, noise_radius, rnd, walls):
+    """ alter the position """
+
+    # extracting from walls the maximum width and height
+    max_walls = max(walls)
+    min_walls = min(walls)
+
+    # get a list of possible positions
+    x_min, x_max = bot_pos[0] - noise_radius, bot_pos[0] + noise_radius
+    y_min, y_max = bot_pos[1] - noise_radius, bot_pos[1] + noise_radius
+
+    # filter them so the we return no positions outsided the maze
+    if x_min < 0:
+        x_min = 1
+    if x_max > max_walls[0]:
+        x_max = max_walls[0]
+    if y_min < 0:
+        y_min = 1
+    if y_max > max_walls[1]:
+        y_max = max_walls[1]
+
+    possible_positions = [
+        (i, j)
+        for i in range(x_min, x_max)
+        for j in range(y_min, y_max)
+        if manhattan_dist((i, j), bot_pos) <= noise_radius
+    ]
+
+    # shuffle the list of positions
+    rnd.shuffle(possible_positions)
+    final_pos = bot_pos
+    noisy = False
+    for pos in possible_positions:
+        # check that the bot won't returned as positioned on a wall square
+        if pos in walls:
+            continue
+        else:
+            final_pos = pos
+            noisy = True
+            break
+            # return the final_pos and a flag if it is noisy or not
+    return [final_pos, noisy]
+    # return [(0,0), True]
+
+
 def manhattan_dist(pos1, pos2):
     """ Manhattan distance between two points.
 
@@ -156,4 +166,4 @@ def manhattan_dist(pos1, pos2):
     manhattan_dist : int
         Manhattan distance between two points
     """
-    return abs(pos1[0]-pos2[0]) + abs(pos1[1]-pos2[1])
+    return abs(pos1[0] - pos2[0]) + abs(pos1[1] - pos2[1])
--- test/test_filter_gamestates.py
+++ test/test_filter_gamestates.py
@@ -3,273 +3,294 @@ from pelita import layout as lt
 import pytest
 import random
 
-def make_gamestate():
-	
-	# get layout
-	layout = (
-		""" 
-		##################
-		#. ... .##.     3#
-		# # #  .  .### #1#
-		# # ##.   .      #
-		#      .   .## # #
-		#0# ###.  .  # # #
-		#2     .##. ... .#
-		################## """)
-		
-	lt_dict = lt.parse_layout(layout)
 
-	
-	# prep input
-	gamestate = {
-			 "turn": 1,
-			 "round": 3,
-			 "max_round": 300,
-			 "walls": lt_dict["walls"],
-			 "food": lt_dict["food"],
-			 "bots": lt_dict["bots"],
-			 "timeouts": [2,3],
-			 "gameover": False,
-			 "whowins": None,
-			 "team_names": ["even","odd"],
-			 "team_say": 'pos!!',
-			 "score": [20,10],
-			 "deaths": [2,3],
-			 "noisy": [False] * 4
-			 }
-			 
-	return(gamestate)
-	
-def sub_test_noiser(new_bots,old_bots,turn,should_noise,test_other):
-	""" sub test fuction to check if noiser worked
-	
-	Parameters
-	----------
-	new_bots: 		bots after noising
-	old_bots:		bots before noising
-	turn: 			which turn is it now? 0,1,2,3
-	should_noise:	should the noiser do something right now, or return same bots?
-	test_other:		true: then it checks if what was meant to happen to other bots happened
-					and vice versa
-					
-	Returns
-	-------
-	a boolean
-	"""
-	
-	if test_other:
-		if not turn % 2:
-			# even case
-			if should_noise:
-				test_bot1 = (not old_bots[1] == new_bots[1])
-				test_bot3 = (not old_bots[3] == new_bots[3])
-				return(test_bot1 or test_bot3)
-			else:
-				test_bot1 = (old_bots[1] == new_bots[1])
-				test_bot3 = (old_bots[3] == new_bots[3])
-				return(test_bot1 and test_bot3)
-		else:
-			if should_noise:
-				test_bot0 = (not old_bots[0] == new_bots[0])
-				test_bot2 = (not old_bots[2] == new_bots[2])
-				return(test_bot0 or test_bot2)
-			else:
-				test_bot0 = (old_bots[0] == new_bots[0])
-				test_bot2 = (old_bots[2] == new_bots[2])
-				return(test_bot0 and test_bot2)
-	else:
-		# test_own should always mean no change
-		if turn % 2:
-			test_bot0 = (old_bots[0] == new_bots[0])
-			test_bot2 = (old_bots[2] == new_bots[2])
-			return(test_bot0 and test_bot2)
-		else:
-			test_bot1 = (old_bots[1] == new_bots[1])
-			test_bot3 = (old_bots[3] == new_bots[3])
-			return(test_bot1 and test_bot3)
+def make_gamestate():
+
+    # get layout
+    layout = """
+        ##################
+        #. ... .##.     3#
+        # # #  .  .### #1#
+        # # ##.   .      #
+        #      .   .## # #
+        #0# ###.  .  # # #
+        #2     .##. ... .#
+        ################## """
+
+    lt_dict = lt.parse_layout(layout)
+
+    # prep input
+    gamestate = {
+        "turn": 1,
+        "round": 3,
+        "max_round": 300,
+        "walls": lt_dict["walls"],
+        "food": lt_dict["food"],
+        "bots": lt_dict["bots"],
+        "timeouts": [2, 3],
+        "gameover": False,
+        "whowins": None,
+        "team_names": ["even", "odd"],
+        "team_say": "pos!!",
+        "score": [20, 10],
+        "deaths": [2, 3],
+        "noisy": [False] * 4,
+    }
+
+    return gamestate
+
+
+def sub_test_noiser(new_bots, old_bots, turn, should_noise, test_other):
+    """ sub test fuction to check if noiser worked
+
+    Parameters
+    ----------
+    new_bots: bots after noising
+    old_bots: bots before noising
+    turn: which turn is it now? 0,1,2,3
+    should_noise: hould the noiser do something right now, or return same bots?
+    test_other: true: then it checks if what was meant to happen to other bots happened
+                                    and vice versa
+
+    Returns
+    -------
+    a boolean
+    """
+
+    if test_other:
+        if not turn % 2:
+            # even case
+            if should_noise:
+                test_bot1 = not old_bots[1] == new_bots[1]
+                test_bot3 = not old_bots[3] == new_bots[3]
+                return test_bot1 or test_bot3
+            else:
+                test_bot1 = old_bots[1] == new_bots[1]
+                test_bot3 = old_bots[3] == new_bots[3]
+                return test_bot1 and test_bot3
+        else:
+            if should_noise:
+                test_bot0 = not old_bots[0] == new_bots[0]
+                test_bot2 = not old_bots[2] == new_bots[2]
+                return test_bot0 or test_bot2
+            else:
+                test_bot0 = old_bots[0] == new_bots[0]
+                test_bot2 = old_bots[2] == new_bots[2]
+                return test_bot0 and test_bot2
+    else:
+        # test_own should always mean no change
+        if turn % 2:
+            test_bot0 = old_bots[0] == new_bots[0]
+            test_bot2 = old_bots[2] == new_bots[2]
+            return test_bot0 and test_bot2
+        else:
+            test_bot1 = old_bots[1] == new_bots[1]
+            test_bot3 = old_bots[3] == new_bots[3]
+            return test_bot1 and test_bot3
+
 
 def test_noiser_no_negative_coordinates():
 
-	gamestate = make_gamestate()
-	old_bots = gamestate["bots"]
-	new_gamestate = gf.noiser(gamestate, noise_radius=5, sight_distance=5, seed=None)
-	new_bots = new_gamestate["bots"]
-	test_1 = all(item[0] > 0 for item in new_bots)
-	test_2 = all(item[1] > 0 for item in new_bots)
-	
-	assert(test_1 and test_2)
-	
+    gamestate = make_gamestate()
+    old_bots = gamestate["bots"]
+    new_gamestate = gf.noiser(gamestate, noise_radius=5, sight_distance=5, seed=None)
+    new_bots = new_gamestate["bots"]
+    test_1 = all(item[0] > 0 for item in new_bots)
+    test_2 = all(item[1] > 0 for item in new_bots)
+
+    assert test_1 and test_2
+
+
 def test_noiser_noising_odd_turn1():
 
-	""" It is the odd team's turn, and the noiser should
-		work on even bots """
-	
-	test_collect_bot0 = []
-	test_collect_bot2 = []
-	for ii in range(10):
-	
-		# we let it run 10 times because it could return the original position,
-		# but in most cases it should return a different pos (due to noise)
-		gamestate = make_gamestate()
-		old_bots = gamestate["bots"]
-		new_gamestate = gf.noiser(gamestate, noise_radius=5, sight_distance=5, seed=None)
-		new_bots = new_gamestate["bots"]
-		test_collect_bot0.append((not old_bots[0] == new_bots[0]))
-		test_collect_bot2.append((not old_bots[2] == new_bots[2]))
-	
-	assert(any(test_collect_bot0) or any(test_collect_bot2))
-	
+    """ It is the odd team's turn, and the noiser should
+    work on even bots """
+
+    test_collect_bot0 = []
+    test_collect_bot2 = []
+    for ii in range(10):
+
+        # we let it run 10 times because it could return the original position,
+        # but in most cases it should return a different pos (due to noise)
+        gamestate = make_gamestate()
+        old_bots = gamestate["bots"]
+        new_gamestate = gf.noiser(
+            gamestate, noise_radius=5, sight_distance=5, seed=None
+        )
+        new_bots = new_gamestate["bots"]
+        test_collect_bot0.append((not old_bots[0] == new_bots[0]))
+        test_collect_bot2.append((not old_bots[2] == new_bots[2]))
+
+    assert any(test_collect_bot0) or any(test_collect_bot2)
+
+
 def test_noiser_noising_odd_turn3():
 
-	""" It is the odd team's turn, and the noiser should
-		work on even bots """
-	
-	test_collect_bot0 = []
-	test_collect_bot2 = []
-	for ii in range(10):
-	
-		# we let it run 10 times because it could return the original position,
-		# but in most cases it should return a different pos (due to noise)
-		gamestate = make_gamestate()
-		gamestate["turn"] = 3
-		old_bots = gamestate["bots"]
-		new_gamestate = gf.noiser(gamestate, noise_radius=5, sight_distance=5, seed=None)
-		new_bots = new_gamestate["bots"]
-		test_collect_bot0.append((not old_bots[0] == new_bots[0]))
-		test_collect_bot2.append((not old_bots[2] == new_bots[2]))
-	
-	assert(any(test_collect_bot0) or any(test_collect_bot2))
-	
+    """ It is the odd team's turn, and the noiser should
+    work on even bots """
+
+    test_collect_bot0 = []
+    test_collect_bot2 = []
+    for ii in range(10):
+
+        # we let it run 10 times because it could return the original position,
+        # but in most cases it should return a different pos (due to noise)
+        gamestate = make_gamestate()
+        gamestate["turn"] = 3
+        old_bots = gamestate["bots"]
+        new_gamestate = gf.noiser(
+            gamestate, noise_radius=5, sight_distance=5, seed=None
+        )
+        new_bots = new_gamestate["bots"]
+        test_collect_bot0.append((not old_bots[0] == new_bots[0]))
+        test_collect_bot2.append((not old_bots[2] == new_bots[2]))
+
+    assert any(test_collect_bot0) or any(test_collect_bot2)
+
+
 def test_noiser_noising_even_turn0():
 
-	""" It is the even team's turn, and the noiser should
-		work on odd bots """
-	
-	test_collect_bot1 = []
-	test_collect_bot3 = []
-	for ii in range(10):
-	
-		# we let it run 10 times because it could return the original position,
-		# but in most cases it should return a different pos (due to noise)
-		gamestate = make_gamestate()
-		gamestate["turn"] = 0
-		old_bots = gamestate["bots"]
-		new_gamestate = gf.noiser(gamestate, noise_radius=5, sight_distance=5, seed=None)
-		new_bots = new_gamestate["bots"]
-		test_collect_bot1.append((not old_bots[1] == new_bots[1]))
-		test_collect_bot3.append((not old_bots[3] == new_bots[3]))
-	
-	assert(any(test_collect_bot1) or any(test_collect_bot3))
-	
+    """ It is the even team's turn, and the noiser should
+    work on odd bots """
+
+    test_collect_bot1 = []
+    test_collect_bot3 = []
+    for ii in range(10):
+
+        # we let it run 10 times because it could return the original position,
+        # but in most cases it should return a different pos (due to noise)
+        gamestate = make_gamestate()
+        gamestate["turn"] = 0
+        old_bots = gamestate["bots"]
+        new_gamestate = gf.noiser(
+            gamestate, noise_radius=5, sight_distance=5, seed=None
+        )
+        new_bots = new_gamestate["bots"]
+        test_collect_bot1.append((not old_bots[1] == new_bots[1]))
+        test_collect_bot3.append((not old_bots[3] == new_bots[3]))
+
+    assert any(test_collect_bot1) or any(test_collect_bot3)
+
+
 def test_noiser_noising_even_turn2():
 
-	""" It is the even team's turn, and the noiser should
-		work on odd bots """
-	
-	test_collect_bot1 = []
-	test_collect_bot3 = []
-	for ii in range(10):
-	
-		# we let it run 10 times because it could return the original position,
-		# but in most cases it should return a different pos (due to noise)
-		gamestate = make_gamestate()
-		gamestate["turn"] = 2
-		old_bots = gamestate["bots"]
-		new_gamestate = gf.noiser(gamestate, noise_radius=5, sight_distance=5, seed=None)
-		new_bots = new_gamestate["bots"]
-		test_collect_bot1.append((not old_bots[1] == new_bots[1]))
-		test_collect_bot3.append((not old_bots[3] == new_bots[3]))
-	
-	assert(any(test_collect_bot1) or any(test_collect_bot3))
-	
+    """ It is the even team's turn, and the noiser should
+    work on odd bots """
+
+    test_collect_bot1 = []
+    test_collect_bot3 = []
+    for ii in range(10):
+
+        # we let it run 10 times because it could return the original position,
+        # but in most cases it should return a different pos (due to noise)
+        gamestate = make_gamestate()
+        gamestate["turn"] = 2
+        old_bots = gamestate["bots"]
+        new_gamestate = gf.noiser(
+            gamestate, noise_radius=5, sight_distance=5, seed=None
+        )
+        new_bots = new_gamestate["bots"]
+        test_collect_bot1.append((not old_bots[1] == new_bots[1]))
+        test_collect_bot3.append((not old_bots[3] == new_bots[3]))
+
+    assert any(test_collect_bot1) or any(test_collect_bot3)
+
+
 def test_noiser_not_noising_own_team_even0():
 
-	""" It is the even team's turn, and the noiser should
-		not work on own bots """
-	
-	gamestate = make_gamestate()
-	gamestate["turn"] = 0
-	old_bots = gamestate["bots"]
-	new_gamestate = gf.noiser(gamestate, noise_radius=5, sight_distance=5, seed=None)
-	new_bots = new_gamestate["bots"]
-	test_bot0 = (old_bots[0] == new_bots[0])
-	test_bot2 = (old_bots[2] == new_bots[2])
-	
-	assert(test_bot0 or test_bot2)
-	
+    """ It is the even team's turn, and the noiser should
+    not work on own bots """
+
+    gamestate = make_gamestate()
+    gamestate["turn"] = 0
+    old_bots = gamestate["bots"]
+    new_gamestate = gf.noiser(gamestate, noise_radius=5, sight_distance=5, seed=None)
+    new_bots = new_gamestate["bots"]
+    test_bot0 = old_bots[0] == new_bots[0]
+    test_bot2 = old_bots[2] == new_bots[2]
+
+    assert test_bot0 or test_bot2
+
+
 def test_noiser_not_noising_own_team_even2():
 
-	""" It is the even team's turn, and the noiser should
-		not work on own bots """
-	
-	gamestate = make_gamestate()
-	gamestate["turn"] = 2
-	old_bots = gamestate["bots"]
-	new_gamestate = gf.noiser(gamestate, noise_radius=5, sight_distance=5, seed=None)
-	new_bots = new_gamestate["bots"]
-	test_bot0 = (old_bots[0] == new_bots[0])
-	test_bot2 = (old_bots[2] == new_bots[2])
-	
-	assert(test_bot0 or test_bot2)
-	
+    """ It is the even team's turn, and the noiser should
+    not work on own bots """
+
+    gamestate = make_gamestate()
+    gamestate["turn"] = 2
+    old_bots = gamestate["bots"]
+    new_gamestate = gf.noiser(gamestate, noise_radius=5, sight_distance=5, seed=None)
+    new_bots = new_gamestate["bots"]
+    test_bot0 = old_bots[0] == new_bots[0]
+    test_bot2 = old_bots[2] == new_bots[2]
+
+    assert test_bot0 or test_bot2
+
+
 def test_noiser_not_noising_own_team_odd1():
 
-	""" It is the odd team's turn, and the noiser should
-		not work on own bots """
-	
-	gamestate = make_gamestate()
-	gamestate["turn"] = 1
-	old_bots = gamestate["bots"]
-	new_gamestate = gf.noiser(gamestate, noise_radius=5, sight_distance=5, seed=None)
-	new_bots = new_gamestate["bots"]
-	test_bot1 = (old_bots[1] == new_bots[1])
-	test_bot3 = (old_bots[3] == new_bots[3])
-	
-	assert(test_bot1 or test_bot3)
-	
+    """ It is the odd team's turn, and the noiser should
+    not work on own bots """
+
+    gamestate = make_gamestate()
+    gamestate["turn"] = 1
+    old_bots = gamestate["bots"]
+    new_gamestate = gf.noiser(gamestate, noise_radius=5, sight_distance=5, seed=None)
+    new_bots = new_gamestate["bots"]
+    test_bot1 = old_bots[1] == new_bots[1]
+    test_bot3 = old_bots[3] == new_bots[3]
+
+    assert test_bot1 or test_bot3
+
+
 def test_noiser_not_noising_own_team_odd3():
 
-	""" It is the odd team's turn, and the noiser should
-		not work on own bots """
-	
-	gamestate = make_gamestate()
-	gamestate["turn"] = 3
-	old_bots = gamestate["bots"]
-	new_gamestate = gf.noiser(gamestate, noise_radius=5, sight_distance=5, seed=None)
-	new_bots = new_gamestate["bots"]
-	test_bot1 = (old_bots[1] == new_bots[1])
-	test_bot3 = (old_bots[3] == new_bots[3])
-	
-	assert(test_bot1 or test_bot3)
-	
+    """ It is the odd team's turn, and the noiser should
+    not work on own bots """
+
+    gamestate = make_gamestate()
+    gamestate["turn"] = 3
+    old_bots = gamestate["bots"]
+    new_gamestate = gf.noiser(gamestate, noise_radius=5, sight_distance=5, seed=None)
+    new_bots = new_gamestate["bots"]
+    test_bot1 = old_bots[1] == new_bots[1]
+    test_bot3 = old_bots[3] == new_bots[3]
+
+    assert test_bot1 or test_bot3
+
+
 def test_noiser_not_noising_at_noise_radius0():
 
-	""" It is the any team's turn, and the noiser should
-		not do anything, because noise radius is 0 """
+    """ It is the any team's turn, and the noiser should
+    not do anything, because noise radius is 0 """
 
-	test_collect = []
-	for tt in range(4):
-		gamestate = make_gamestate()
-		gamestate["turn"] = tt
-		old_bots = gamestate["bots"]
-		new_gamestate = gf.noiser(gamestate, noise_radius=0, sight_distance=5, seed=None)
-		new_bots = new_gamestate["bots"]
-		sub_test_collect = []
-		for ss in range(4):
-			sub_test_collect.append((old_bots[1] == new_bots[1]))
-		sub_test_collect.append(all(sub_test_collect))
-		
-	assert(all(sub_test_collect))
+    test_collect = []
+    for tt in range(4):
+        gamestate = make_gamestate()
+        gamestate["turn"] = tt
+        old_bots = gamestate["bots"]
+        new_gamestate = gf.noiser(
+            gamestate, noise_radius=0, sight_distance=5, seed=None
+        )
+        new_bots = new_gamestate["bots"]
+        sub_test_collect = []
+        for ss in range(4):
+            sub_test_collect.append((old_bots[1] == new_bots[1]))
+        sub_test_collect.append(all(sub_test_collect))
 
-@pytest.mark.parametrize('ii',range(30))	
+    assert all(sub_test_collect)
+
+
+@pytest.mark.parametrize("ii", range(30))
 def test_noiser_noising_at_noise_radius_extreme(ii):
 
-	""" It is the any team's turn, and the noiser should
-		still noise within confines of maze, despite extreme radius """
-		
-	gamestate = make_gamestate()
-	gamestate["turn"] = random.randint(0,3)
-	old_bots = gamestate["bots"]
-	new_gamestate = gf.noiser(gamestate, noise_radius=50, sight_distance=5, seed=None)
-	new_bots = new_gamestate["bots"]
-	assert sub_test_noiser(new_bots,old_bots,gamestate["turn"],True,True)
+    """ It is the any team's turn, and the noiser should
+    still noise within confines of maze, despite extreme radius """
+
+    gamestate = make_gamestate()
+    gamestate["turn"] = random.randint(0, 3)
+    old_bots = gamestate["bots"]
+    new_gamestate = gf.noiser(gamestate, noise_radius=50, sight_distance=5, seed=None)
+    new_bots = new_gamestate["bots"]
+    assert sub_test_noiser(new_bots, old_bots, gamestate["turn"], True, True)
