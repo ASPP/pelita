fa39cf2b807658cdcfb91269bdd6f63298ed8464
Lisa Schwetlick <mail@lisaschwetlick.de>
2019-06-03 19:36:28 +0200

    update move to position in `demo02_random.py` and `test_demo02_random.py`

--- template/demo02_random.py
+++ template/demo02_random.py
@@ -6,5 +6,5 @@ TEAM_NAME = 'RandomBots'
 def move(bot, state):
     # make a reference to our bot with a shorter name
     # mostly useful for longer code, of course
-    next_move = bot.random.choice(bot.legal_positions)
-    return next_move, state
+    next_pos = bot.random.choice(bot.legal_positions)
+    return next_pos, state
