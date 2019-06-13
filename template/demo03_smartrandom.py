f42d04e1067f3a4bbc2f4e74482a946085c717c1
Tiziano Zito <opossumnano@gmail.com>
2019-06-13 16:12:34 +0200

    refactor smartrandom bot to be easier to understand

--- template/demo03_smartrandom.py
+++ template/demo03_smartrandom.py
@@ -6,8 +6,9 @@
 TEAM_NAME = 'SmartRandomBots'
 
 def move(bot, state):
+    enemy = bot.enemy
     # get a tuple with our enemy positions
-    enemy_pos = (bot.enemy[0].position, bot.enemy[1].position)
+    enemy_pos = (enemy[0].position, enemy[1].position)
 
     # sensible moves are all legal moves that do not cause death
     sensible_positions = bot.legal_positions[:]
@@ -16,7 +17,11 @@ def move(bot, state):
         if (next_pos in enemy_pos) and (next_pos not in bot.homezone):
             sensible_positions.remove(next_pos)
 
-    if len(sensible_positions) != 0:
+    if len(sensible_positions) == 0:
+        # we can't go anywhere safe
+        # so just stop here and hope for the best
+        next_pos = bot.position
+    else:
         # we can do something without risk
 
         # collect positions that have something interesting in it (food or enemy to
@@ -28,14 +33,11 @@ def move(bot, state):
             # the new position is interesting if
             # 1. we are in our homezone and we can eat an enemy
             cond1 = (new_pos in bot.homezone) and (new_pos in enemy_pos)
-            # 2. we are in enemy's homezone and we can eat food
-            enemy_home = bot.enemy[0].homezone
-            food = bot.enemy[0].food
-            cond2 = (new_pos in enemy_home) and (new_pos in food)
+            # or, if 2. we are in enemy's homezone and we can eat food
+            cond2 = (new_pos in enemy[0].homezone) and (new_pos in enemy[0].food)
             if cond1 or cond2:
-                    # either one condition is met, this position is interesting
+                    # if either one condition is met, this position is interesting
                     interesting_positions.append(new_pos)
-        # now we have scanned all sensible moves.
         # do we have any interesting moves to do?
         if len(interesting_positions) > 0:
             # yes, so choose one at random
@@ -49,9 +51,5 @@ def move(bot, state):
             # if we don't break out of the loop early it means that all new
             # positions have been visited already, so we just pick the
             # last one in the list
-    else:
-        # we can't go anywhere safe
-        # so just stop here and hope for the best
-        next_pos= bot.position
 
     return next_pos, state
