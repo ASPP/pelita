3432a9f20c5cdda441f3d0d642ec52230c1912f2
Tiziano Zito <opossumnano@gmail.com>
2019-06-16 18:34:32 +0200

    simplified debugger bot (we require Py3.7 now anyway)

--- template/demo08_debugger.py
+++ template/demo08_debugger.py
@@ -2,13 +2,11 @@
 # IMPORTANT: timeouts need to be disabled, or you will not have time to use
 # the debugger at all. Run a game with:
 # pelita --no-timeout demo08_debugger.py demo01_stopping.py
-#
+# The debugger works automatically also inside of pytest
 
 TEAM_NAME = 'Debuggable Bot'
 
 def move(bot, state):
-    # in Python >= 3.7 this can be changed to simply
-    # breakpoint()
-    import pdb; pdb.set_trace()
+    breakpoint()
     next_pos = (0,0)
     return next_pos, state
