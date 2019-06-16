9681642220aa3a2a35d1eeaf1b3429a56b21c09c
Tiziano Zito <opossumnano@gmail.com>
2019-06-16 18:36:18 +0200

    reworded docs of polite random

--- template/demo09_polite_random.py
+++ template/demo09_polite_random.py
@@ -5,15 +5,15 @@ TEAM_NAME = 'PoliteRandomBots'
 
 def move(bot, state):
 
-    possible_moves = []
+    possible_positions = []
     for pos in bot.legal_positions:
         if pos == bot.other.position:
             bot.say('Excuse me. Sorry. Excuse me.')
         else:
-            possible_moves.append(pos)
-    # possible_moves is never empty, because we can either:
+            possible_positions.append(pos)
+    # possible_positions is never empty, because we can either:
     # - move away from our team mate if we already are in the same position, or
-    # - we can stop if our only other legal move would put us in the same
+    # - we can stop if our only other legal position would put us in the same
     #   position as our team mate
-    next_pos = bot.random.choice(possible_moves)
+    next_pos = bot.random.choice(possible_positions)
     return next_pos, state
