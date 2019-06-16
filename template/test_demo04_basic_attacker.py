d6dc3ddc6543732cd00039dfdb6d7bc28de0bdc9
Tiziano Zito <opossumnano@gmail.com>
2019-06-16 18:08:02 +0200

    refactor basick_attacker not to use bot.track but instead...
    ... forget about target food and backtrack on safe positions only

--- template/test_demo04_basic_attacker.py
+++ template/test_demo04_basic_attacker.py
@@ -22,11 +22,20 @@ def test_no_kamikaze():
     ########
     """
     bot = setup_test_game(layout=layout, is_blue=True)
-    # create a "fake" track of previous moves, as our bot needs it to decide
-    # where to go to avoid a bot
-    bot.track = [(4,2), (5,2)]
     next_move, _ = move(bot, None)
-    assert next_move == (4, 2)
+    assert next_move == (4, 2) or next_move == (5, 2)
+
+def test_do_not_step_on_enemy():
+    # check that we don't step back on an enemy when we are fleeing
+    layout="""
+    ########
+    #    E.#
+    #.1 #0E#
+    ########
+    """
+    bot = setup_test_game(layout=layout, is_blue=True)
+    next_move, _ = move(bot, None)
+    assert next_move == (5, 2)
 
 def test_shortest_path():
     # is the Graph implementation in pelita giving us the shortest path to the
