ef7d28348ad18bed42a16abc9febb8d1cfb4f73b
Tiziano Zito <opossumnano@gmail.com>
2019-06-16 18:13:12 +0200

    rephrase docs of one_and_one

--- template/demo06_one_and_one.py
+++ template/demo06_one_and_one.py
@@ -5,7 +5,7 @@ from demo05_basic_defender import move as move_defender
 from demo04_basic_attacker import move as move_attacker
 
 def move(bot, state):
-    # create a combined game state to collect the states for both bots
+    # create a combined state to collect the states for both bots
     if state is None:
         # initialization
         state = {'attacker' : None, 'defender' : None}
