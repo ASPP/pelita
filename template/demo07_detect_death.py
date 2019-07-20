0920b0fb37394430ad49c28a8f8a91f400f695c2
Tiziano Zito <opossumnano@gmail.com>
2019-07-20 21:00:26 +0200

    use new bot.deaths attribute in detect_death demo

--- template/demo07_detect_death.py
+++ template/demo07_detect_death.py
@@ -1,24 +1,14 @@
 # This bots moves randomly and if it gets eaten on the way, it cries out loud
 TEAM_NAME = "Death Detectors Bots"
 
-MESSAGE = 'I am a zombie now!'
-EATEN_INERTIA = 10
+MESSAGE = 'I was killed '
 
 def move(bot, state):
+    # update the death count for this bot
+    ndeaths = sum(bot.deaths)
 
-    if state is None:
-        # initialize a state dictionary for both bots
-        state = {0:0, 1:0}
-
-    # check if we have been eaten in the previous round
-    if bot.eaten:
-        # set the speak inertia
-        state[bot.turn] = EATEN_INERTIA
-
-    if state[bot.turn]:
-        # speak for as many rounds as EATEN_INTERTIA
-        bot.say(MESSAGE)
-        state[bot.turn] -= 1
+    if ndeaths > 0:
+        bot.say(MESSAGE + f'{ndeaths} time(s)!')
 
     # copy the available positions, so that we can use random.shuffle,
     # which unfortunately shuffles lists in-place.
