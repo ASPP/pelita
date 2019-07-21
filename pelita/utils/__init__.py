97c89644d757496a492e283967f00994b4ad2565
Tiziano Zito <opossumnano@gmail.com>
2019-07-21 14:54:23 +0200

    round=None makes no sense

--- pelita/utils/__init__.py
+++ pelita/utils/__init__.py
@@ -69,7 +69,7 @@ def setup_test_game(*, layout, game=None, is_blue=True, round=None, score=None, 
     bot = make_bots(walls=layout.walls[:],
                     team=team,
                     enemy=enemy,
-                    round=None,
+                    round=round,
                     bot_turn=0,
                     rng=rng)
     return bot
