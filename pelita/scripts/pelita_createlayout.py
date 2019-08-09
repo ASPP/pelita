ee3f33471d6a926fd8be90531927f036fa8dc2dc
Tiziano Zito <opossumnano@gmail.com>
2019-08-09 19:46:04 +0200

    DOC: added example shell script to re-create all built-in pelita layouts

--- pelita/scripts/pelita_createlayout.py
+++ pelita/scripts/pelita_createlayout.py
@@ -14,14 +14,43 @@ WIDTH = 32
 FOOD = 30
 SEED = None
 
+description="""
+Return a new random layout to be used by pelita
+"""
+epilog=f"""
+NOTE: To re-create the layouts used in the installed pelita, you can use the
+following shell script:
+
+# initialize random seed, any subsequent evaluation of $RANDOM will get
+# a different, but replicable random number
+RANDOM=39285
+
+# numbers of layouts to generate per size
+N=100
+
+for COUNT in $(seq -w 1 $N); do
+    echo "Generating normal_$COUNT..."
+    pelita-createlayout -y {HEIGHT} -x {WIDTH} -f {FOOD} -s $RANDOM > normal_${{COUNT}}.layout
+done
+for COUNT in $(seq -w 1 $N); do
+    echo "Generating small_$COUNT..."
+    pelita-createlayout -y {HEIGHT//2} -x {WIDTH//2} -f {FOOD//3} -s $RANDOM > small_${{COUNT}}.layout
+done
+for COUNT in $(seq -w 1 $N); do
+    echo "Generating big_$COUNT..."
+    pelita-createlayout -y {HEIGHT*2} -x {WIDTH*22} -f {FOOD*2} -s $RANDOM > big_${{COUNT}}.layout
+done
+
+"""
+
 def default(text, default):
     return text + f' (default: {default})'
 
-
-
 def main():
     parser = argparse.ArgumentParser(
-                  description='Return a new random layout to be used by pelita',
+                  description=description,
+                  epilog=epilog,
+                  formatter_class=argparse.RawDescriptionHelpFormatter,
                   add_help=True)
 
     parser.add_argument('--height', '-y', default=HEIGHT, type=int, metavar='Y',
