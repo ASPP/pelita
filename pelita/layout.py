1b0d3d1d024595f6e9ec17ec5bafce540335083d
Lisa Schwetlick <mail@lisaschwetlick.de>
2020-04-22 16:04:20 +0200

    got bot names wrong

--- pelita/layout.py
+++ pelita/layout.py
@@ -243,7 +243,7 @@ def parse_layout(layout_str, food=None, bots=None):
         else:
             lfood = sorted(list(set(food + lfood)))
 
-    if not (set(bots.keys()) <= {"a", "b", "c", "d"}):
+    if not (set(bots.keys()) <= {"a", "b", "x", "y"}):
         raise ValueError(f"Invalid Bot names in {bots}.")
 
     # check if additional bots are on legal positions
