0ea45ed9a0e83d515a9d6289fe7a2a43178ef1dc
Rike-Benjamin Schuppner <rikebs@debilski.de>
2020-06-24 00:11:54 +0200

    ENH: Less duplication in benchmark.

--- demo/benchmark_game.py
+++ demo/benchmark_game.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import argparse
+import functools
 import timeit
 
 from pelita.layout import parse_layout
@@ -30,22 +31,14 @@ LAYOUT="""
 
 layout = parse_layout(LAYOUT)
 
-def run_stopping():
-    return run_game([stopping_player, stopping_player], max_rounds=300, layout_dict=layout, print_result=False, allow_exceptions=True)
-
-def run_nqrandom():
-    return run_game([nq_random_player, nq_random_player], max_rounds=300, layout_dict=layout, print_result=False, allow_exceptions=True)
-
-def run_stopping_remote():
-    return run_game(["pelita/player/StoppingPlayer.py", "pelita/player/StoppingPlayer.py"], max_rounds=300, layout_dict=layout, print_result=False, allow_exceptions=True)
-
-def run_nqrandom_remote():
-    return run_game(["pelita/player/RandomPlayers.py", "pelita/player/RandomPlayers.py"], max_rounds=300, layout_dict=layout, print_result=False, allow_exceptions=True)
+def run(teams, max_rounds):
+    return run_game(teams, max_rounds=max_rounds, layout_dict=layout, print_result=False, allow_exceptions=True)
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Benchmark pelita run_game')
     parser.add_argument('--repeat', help="Number of repeats of timeit.", default=5, type=int)
     parser.add_argument('--number', help="Number of iterations inside timeit.", default=10, type=int)
+    parser.add_argument('--max-rounds', help="Max rounds.", default=300, type=int)
 
     return parser.parse_args()
 
@@ -53,16 +46,17 @@ if __name__ == '__main__':
     args = parse_args()
     REPEAT = args.repeat
     NUMBER = args.number
+    MAX_ROUNDS = args.max_rounds
 
-    print(f"Running {NUMBER} times. Fastest out of {REPEAT}:")
+    print(f"Running {NUMBER} times with max {MAX_ROUNDS} rounds. Fastest out of {REPEAT}:")
     tests = [
-        ("Stopping", run_stopping),
-        ("NQ_Random", run_nqrandom),
-        ("Stopping (remote)", run_stopping_remote),
-        ("NQ_Random (remote)", run_nqrandom_remote),
+        ("Stopping", [stopping_player, stopping_player]),
+        ("NQ_Random", [nq_random_player, nq_random_player]),
+        ("Stopping (remote)", ["pelita/player/StoppingPlayer.py", "pelita/player/StoppingPlayer.py"]),
+        ("NQ_Random (remote)", ["pelita/player/RandomPlayers.py", "pelita/player/RandomPlayers.py"]),
     ]
-    for name, fn in tests:
-        result = min(timeit.repeat(fn, repeat=REPEAT, number=NUMBER))
+    for name, teams in tests:
+        result = min(timeit.repeat(functools.partial(run, teams=teams, max_rounds=MAX_ROUNDS), repeat=REPEAT, number=NUMBER))
         print(f"{name:<20}: {result}")
 
     #import cProfile
