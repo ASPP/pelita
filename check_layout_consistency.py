847bbb028af0dfbd4db5de82327cc53654ea8b5a
Rike-Benjamin Schuppner <rikebs@debilski.de>
2021-07-18 14:41:21 +0200

    RF: Use walls_to_layout in check_layout_consistency

--- check_layout_consistency.py
+++ check_layout_consistency.py
@@ -1,35 +1,10 @@
 """Detect if a layout contains "chambers" with food"""
 import sys
 
-import networkx
+import networkx as nx
 from pelita.layout import parse_layout
+from pelita.utils import walls_to_graph
 
-
-def get_hw(layout):
-    walls = layout['walls']
-    width = max([coord[0] for coord in walls]) + 1
-    height = max([coord[1] for coord in walls]) + 1
-    return height, width
-
-def layout_to_graph(layout):
-    """Return a networkx.Graph object given the layout
-    """
-    graph = networkx.Graph()
-    height, width = get_hw(layout)
-    walls = layout['walls']
-    for x in range(width):
-        for y in range(height):
-            if (x, y) not in walls:
-                # this is a free position, get its neighbors
-                for delta_x, delta_y in ((1,0), (-1,0), (0,1), (0,-1)):
-                    neighbor = (x + delta_x, y + delta_y)
-                    # we don't need to check for getting neighbors out of the maze
-                    # because our mazes are all surrounded by walls, i.e. our
-                    # deltas will not put us out of the maze
-                    if neighbor not in walls:
-                        # this is a genuine neighbor, add an edge in the graph
-                        graph.add_edge((x, y), neighbor)
-    return graph
 
 if __name__ == '__main__':
 
@@ -44,8 +19,8 @@ if __name__ == '__main__':
 
     # first of all, check for symmetry
     # if something is found on the left, it should be found center-mirrored on the right
-    height, width = get_hw(layout)
-    known = layout['walls']+layout['food']
+    width, height = layout['shape']
+    known = layout['walls'] | set(layout['food'])
     layout['empty'] = [(x,y) for x in range(width) for y in range(height) if (x,y) not in known]
     for x in range(width // 2):
         for y in range(height):
@@ -56,14 +31,14 @@ if __name__ == '__main__':
                         print(f'{flname}: Layout is not symmetric {coord} != {cmirror}')
 
 
-    graph = layout_to_graph(layout)
+    graph = walls_to_graph(layout['walls'])
 
     # check for dead_ends
     for node, degree in graph.degree():
         if degree < 2:
             print(f'{flname}: found dead end in {node}')
 
-    if networkx.node_connectivity(graph) < 2:
-        entrance = networkx.minimum_node_cut(graph).pop()
+    if nx.node_connectivity(graph) < 2:
+        entrance = nx.minimum_node_cut(graph).pop()
         print(f'{flname}: Detected chamber, entrance: {entrance}')
 
