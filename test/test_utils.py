1e88c8ec3ff8ce1a6c3291478516d778be28c8e8
Rike-Benjamin Schuppner <debilski@debilski.de>
2023-04-19 15:23:40 +0200

    TST: Some fixes in tests

--- test/test_utils.py
+++ test/test_utils.py
@@ -3,26 +3,6 @@ import pytest
 from pelita import utils
 from pelita.player import stopping_player
 
-@pytest.mark.parametrize('is_blue', [True, False])
-def test_setup_test_game(is_blue):
-    layout = utils.load_builtin_layout('small_001', is_blue=is_blue)
-    test_game = utils.setup_test_game(layout=layout, is_blue=is_blue)
-
-    if is_blue:
-        assert test_game.position == (1, 5)
-        assert test_game.other.position == (1, 6)
-        assert test_game.enemy[0].position == (16, 1)
-        assert test_game.enemy[1].position == (16, 2)
-    else:
-        assert test_game.position == (16, 2)
-        assert test_game.other.position == (16, 1)
-        assert test_game.enemy[0].position == (1, 5)
-        assert test_game.enemy[1].position == (1, 6)
-
-    # load_builtin_layout loads unnoised enemies
-    assert test_game.enemy[0].is_noisy is False
-    assert test_game.enemy[1].is_noisy is False
-
 
 @pytest.mark.parametrize('is_blue', [True, False])
 def test_setup_test_game(is_blue):
