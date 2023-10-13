4ba30512bb585bfa7a19033314655da528d09184
Rike-Benjamin Schuppner <debilski@debilski.de>
2023-10-13 17:26:49 +0200

    RF: Send final game state with to network players
    This makes it easier for backends/middleends to capture the last state
    that a player receives without having to parse it and ensure that it
    actually contains a state.
    
    A corollary to not doing that kind of deep package inspection is that,
    once a game has been started, we cannot distinguish between control
    messages and game info (set_initial/get_move) messages anymore and have
    to assume that everything is a message that needs to be forwarded to the
    backend player (unless we change our ZMQ protocoll to include an
    additional control channel).

--- pelita/game.py
+++ pelita/game.py
@@ -506,12 +506,18 @@ def prepare_bot_state(game_state, idx=None):
     """
 
     bot_initialization = game_state.get('turn') is None and idx is not None
+    bot_finalization = game_state.get('turn') is not None and idx is not None
 
     if bot_initialization:
         # We assume that we are in get_initial phase
         turn = idx
         bot_turn = None
         seed = game_state['rnd'].randint(0, sys.maxsize)
+    elif bot_finalization:
+        # Called for remote players in _exit
+        turn = idx
+        bot_turn = None
+        seed = None
     else:
         turn = game_state['turn']
         bot_turn = game_state['turn'] // 2
@@ -977,9 +983,10 @@ def check_exit_remote_teams(game_state):
     """ If the we are gameover, we want the remote teams to shut down. """
     if game_state['gameover']:
         _logger.info("Gameover. Telling teams to exit.")
-        for team in game_state['teams']:
+        for idx, team in enumerate(game_state['teams']):
             try:
-                team._exit()
+                team_game_state = prepare_bot_state(game_state, idx=idx)
+                team._exit(team_game_state)
             except AttributeError:
                 pass
 
