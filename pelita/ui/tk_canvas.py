ASPP/pelita

commit 92ca5553f61e05c5edaf9f3bd240628bb1ef06f8

RF: Use shape instead of max(walls)+1

pelita/ui/tk_canvas.py
@@ -341,8 +341,7 @@ class TkApplication:
 
 
     def init_mesh(self, game_state):
-        width = max(game_state['walls'])[0] + 1
-        height = max(game_state['walls'])[1] + 1
+        width, height = game_state['shape']
 
         if self.geometry is None:
             screensize = (
@@ -425,8 +424,8 @@ class TkApplication:
         self.size_changed = False
 
     def draw_universe(self, game_state):
-        self.mesh_graph.num_x = max(game_state['walls'])[0] + 1
-        self.mesh_graph.num_y = max(game_state['walls'])[1] + 1
+        self.mesh_graph.num_x = game_state['shape'][0]
+        self.mesh_graph.num_y = game_state['shape'][1]
 
         self.draw_grid()
         self.draw_selected(game_state)
@@ -578,7 +577,7 @@ class TkApplication:
                 has_food = pos in game_state['food']
                 is_wall = pos in game_state['walls']
                 bots = [idx for idx, bot in enumerate(game_state['bots']) if bot==pos]
-                if pos[0] < (max(game_state['walls'])[0] + 1) // 2:
+                if pos[0] <= (game_state['shape'][0] // 2):
                     zone = "blue"
                 else:
                     zone = "red"
@@ -799,6 +798,7 @@ class TkApplication:
         game_state['walls'] = _ensure_tuples(game_state['walls'])
         game_state['food'] = _ensure_tuples(game_state['food'])
         game_state['bots'] = _ensure_tuples(game_state['bots'])
+        game_state['shape'] = tuple(game_state['shape'])
         self.update(game_state)
         if self._stop_after is not None:
             if self._stop_after == 0:

pelita/game.py
@@ -510,6 +510,7 @@ def prepare_bot_state(game_state, idx=None):
     enemy_team = 1 - own_team
     enemy_positions = game_state['bots'][enemy_team::2]
     noised_positions = noiser(walls=game_state['walls'],
+                              shape=game_state['shape'],
                               bot_position=bot_position,
                               enemy_positions=enemy_positions,
                               noise_radius=game_state['noise_radius'],
@@ -743,6 +744,7 @@ def apply_move(gamestate, bot_position):
     score = gamestate["score"]
     food = gamestate["food"]
     walls = gamestate["walls"]
+    shape = gamestate["shape"]
     food = gamestate["food"]
     n_round = gamestate["round"]
     kills = gamestate["kills"]
@@ -760,7 +762,7 @@ def apply_move(gamestate, bot_position):
     team_errors = gamestate["errors"][team]
 
     # the allowed moves for the current bot
-    legal_positions = get_legal_positions(walls, gamestate["bots"][gamestate["turn"]])
+    legal_positions = get_legal_positions(walls, shape, gamestate["bots"][gamestate["turn"]])
 
     # unless we have already made an error, check if we made a legal move
     if not (n_round, turn) in team_errors:
@@ -791,12 +793,11 @@ def apply_move(gamestate, bot_position):
     _logger.info(f"Bot {turn} moves to {bot_position}.")
     # then apply rules
     # is bot in home or enemy territory
-    x_walls = [i[0] for i in walls]
-    boundary = max(x_walls) / 2  # float
+    boundary = gamestate['shape'][0] / 2
     if team == 0:
         bot_in_homezone = bot_position[0] < boundary
     elif team == 1:
-        bot_in_homezone = bot_position[0] > boundary
+        bot_in_homezone = bot_position[0] >= boundary
     # update food list
     if not bot_in_homezone:
         if bot_position in food[1 - team]:

pelita/gamestate_filters.py
@@ -5,7 +5,7 @@ import copy
 ### The main function
 
 
-def noiser(walls, bot_position, enemy_positions, noise_radius=5, sight_distance=5, rnd=None):
+def noiser(walls, shape, bot_position, enemy_positions, noise_radius=5, sight_distance=5, rnd=None):
     """Function to make bot positions noisy in a game state.
 
     Applies uniform noise in maze space. Noise will only be applied if the
@@ -67,7 +67,7 @@ def noiser(walls, bot_position, enemy_positions, noise_radius=5, sight_distance=
 
         if cur_distance is None or cur_distance > sight_distance:
             # If so then alter the position of the enemy
-            new_pos, noisy_flag = alter_pos(b, noise_radius, rnd, walls)
+            new_pos, noisy_flag = alter_pos(b, noise_radius, rnd, walls, shape)
             noised_positions[count] = new_pos
             is_noisy[count] = noisy_flag
         else:
@@ -80,12 +80,8 @@ def noiser(walls, bot_position, enemy_positions, noise_radius=5, sight_distance=
 ### The subfunctions
 
 
-def alter_pos(bot_pos, noise_radius, rnd, walls):
+def alter_pos(bot_pos, noise_radius, rnd, walls, shape):
     """ alter the position """
-
-    # extracting from walls the maximum width and height
-    max_walls = max(walls)
-    min_walls = min(walls)
 
     # get a list of possible positions
     x_min, x_max = bot_pos[0] - noise_radius, bot_pos[0] + noise_radius
@@ -94,12 +90,12 @@ def alter_pos(bot_pos, noise_radius, rnd, walls):
     # filter them so the we return no positions outsided the maze
     if x_min < 0:
         x_min = 1
-    if x_max > max_walls[0]:
-        x_max = max_walls[0]
+    if x_max >= shape[0]:
+        x_max = shape[0] - 1
     if y_min < 0:
         y_min = 1
-    if y_max > max_walls[1]:
-        y_max = max_walls[1]
+    if y_max >= shape[1]:
+        y_max = shape[1] - 1
 
     possible_positions = [
         (i, j)

pelita/layout.py
@@ -380,7 +380,7 @@ def initial_positions(walls):
     return [left[0], right[0], left[1], right[1]]
 
 
-def get_legal_positions(walls, bot_position):
+def get_legal_positions(walls, shape, bot_position):
     """ Returns all legal positions that a bot at `bot_position`
     can go to.
 
@@ -401,7 +401,7 @@ def get_legal_positions(walls, bot_position):
     ValueError
         if bot_position invalid or on wall
     """
-    width, height = wall_dimensions(walls)
+    width, height = shape
     if not (0, 0) <= bot_position < (width, height):
         raise ValueError(f"Position {bot_position} not inside maze ({width}x{height}).")
     if bot_position in walls:

pelita/player/team.py
@@ -401,9 +401,8 @@ def make_team(team_spec, team_name=None, zmq_context=None, idx=None, store_outpu
     return team_player, zmq_context
 
 
-def create_homezones(walls):
-    width = max(walls)[0]+1
-    height = max(walls)[1]+1
+def create_homezones(shape):
+    width, height = shape
     return [
         [(x, y) for x in range(0, width // 2)
                 for y in range(0, height)],
@@ -560,8 +559,7 @@ class Bot:
     def _repr_html_(self):
         """ Jupyter-friendly representation. """
         bot = self
-        width = max(bot.walls)[0] + 1
-        height = max(bot.walls)[1] + 1
+        width, height = bot.shape
 
         with StringIO() as out:
             out.write("<table>")
@@ -595,8 +593,6 @@ class Bot:
 
     def __str__(self):
         bot = self
-        width = max(bot.walls)[0] + 1
-        height = max(bot.walls)[1] + 1
 
         if bot.is_blue:
             blue = bot if not bot.turn else bot.other
@@ -636,7 +632,8 @@ class Bot:
 
             layout = layout_as_str(walls=bot.walls[:],
                                    food=bot.food + bot.enemy[0].food,
-                                   bots=bot_positions)
+                                   bots=bot_positions,
+                                   shape=bot.shape)
 
             out.write(str(layout))
             out.write(footer)
@@ -650,7 +647,7 @@ def make_bots(*, walls, shape, team, enemy, round, bot_turn, rng):
     team_index = team['team_index']
     enemy_index = enemy['team_index']
 
-    homezone = create_homezones(walls)
+    homezone = create_homezones(shape)
     initial_positions = layout.initial_positions(walls)
     team_initial_positions = initial_positions[team_index::2]
     enemy_initial_positions = initial_positions[enemy_index::2]

commit e72022cf5d6a7411770842dea2205fcc362b4fa6

BF: TK UI shows wrong homezone for blue team
Longstanding bug introduced in 92ca5553f61e05c5edaf9f3bd240628bb1ef06f8

The homezone for the blue team shown in the field status (bottom right)
is one square too big. Typical off-by-one error ;-)

pelita/ui/tk_canvas.py
@@ -705,7 +705,7 @@ class TkApplication:
                 has_food = pos in game_state['food']
                 is_wall = pos in game_state['walls']
                 bots = [idx for idx, bot in enumerate(game_state['bots']) if bot==pos]
-                if pos[0] <= (game_state['shape'][0] // 2):
+                if pos[0] < (game_state['shape'][0] // 2):
                     zone = "blue"
                 else:
                     zone = "red"