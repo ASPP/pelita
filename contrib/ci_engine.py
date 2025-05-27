076b13fda9c2b7fb3f0dc3987bf2d23699706ad0
Rike-Benjamin Schuppner <debilski@debilski.de>
2025-05-27 18:25:27 +0200

    ENH: Use shlex.join for pretty printing of shell commands

--- contrib/ci_engine.py
+++ contrib/ci_engine.py
@@ -46,6 +46,7 @@ import configparser
 import itertools
 import json
 import logging
+import shlex
 import sqlite3
 import subprocess
 import sys
@@ -70,7 +71,7 @@ def hash_team(team_spec):
                     'pelita.scripts.pelita_player',
                     'hash-team',
                     team_spec]
-    _logger.debug("Executing: %r", external_call)
+    _logger.debug("Executing: %r", shlex.join(external_call))
     res = subprocess.run(external_call, capture_output=True, text=True)
     return res.stdout.strip().split("\n")[-1].strip()
 
