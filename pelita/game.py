d4db11f98bb44c02f4e1b458014226b8636f7d73
Rike-Benjamin Schuppner <debilski@debilski.de>
2025-05-27 18:25:27 +0200

    ENH: Use shlex.join for pretty printing of shell commands

--- pelita/game.py
+++ pelita/game.py
@@ -3,6 +3,7 @@
 import logging
 import math
 import os
+import shlex
 import subprocess
 import sys
 import time
@@ -93,7 +94,7 @@ class TkViewer:
         external_call = [sys.executable,
                         '-m',
                         tkviewer] + viewer_args
-        _logger.debug("Executing: %r", external_call)
+        _logger.debug("Executing: %r", shlex.join(external_call))
         # os.setsid will keep the viewer from closing when the main process exits
         # a better solution might be to decouple the viewer from the main process
         if _mswindows:
