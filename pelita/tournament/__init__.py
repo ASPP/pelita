15ab3564c9d5335f78c49a54ae6542610728b791
Rike-Benjamin Schuppner <debilski@debilski.de>
2026-04-14 16:26:43 +0200

    NF: Introduce HTTP POST publishing

--- pelita/tournament/__init__.py
+++ pelita/tournament/__init__.py
@@ -12,6 +12,7 @@ import tempfile
 import time
 from dataclasses import dataclass
 
+import httpx
 import yaml
 import zmq
 
@@ -137,7 +138,7 @@ def call_pelita(team_specs, *, rounds, size, viewer, seed, timeout=3, initial_ti
     seed = ['--seed', seed] if seed else []
     timeout = ['--timeout', str(timeout)]
     initial_timeout = ['--initial-timeout', str(initial_timeout)]
-    publish = ['--publish', publish] if publish else []
+    publish = ['--http-post', publish] if publish else []
     write_replay = ['--write-replay', write_replay] if write_replay else []
     store_output = ['--store-output', store_output] if store_output else []
     append_blue = ['--append-blue', team_infos[0]] if team_infos[0] else []
@@ -306,11 +307,9 @@ class Config:
         self.tournament_log_file = None
 
         if self.publish:
-            ctx = zmq.Context()
-            self.socket = ctx.socket(zmq.PUB)
-            self.socket.connect(self.publish)
+            self.http_session = httpx.Client()
         else:
-            self.socket = None
+            self.http_session = None
 
     @property
     def team_ids(self):
@@ -329,13 +328,14 @@ class Config:
         return self.teams[team]["spec"]
 
     def send_remote(self, action, data=None):
-        if not self.socket:
+        if not self.http_session:
             return
+
         if data is None:
             publish_string = {"__action__": action}
         else:
             publish_string = {"__action__": action, "__data__": data}
-        self.socket.send_json(publish_string)
+        self.http_session.post(self.publish, content=json.dumps(publish_string))
 
     def _print(self, *args, **kwargs):
         print(*args, **kwargs)
